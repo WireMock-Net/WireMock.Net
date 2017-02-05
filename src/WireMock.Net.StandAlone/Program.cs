51cc74ad2007dca0feb7d00610624a37aa1536d6
Stef Heyenrath <stef.heyenrath@gmail.com>
2017-02-05 18:43:01 +0100

    added WireMock.Net.StandAlone

---
+++ src/WireMock.Net.StandAlone/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+using WireMock.Server;
+
+namespace WireMock.Net.StandAlone
+{
+    public class Program
+    {
+        private class Options
+        {
+            [ValueArgument(typeof(string), 'u', "Urls", Description = "URL(s) to listen on", Optional = false, AllowMultiple = true)]
+            public string[] Urls;
+
+            [SwitchArgument('p', "AllowPartialMapping", true, Description = "Allow Partial Mapping (default set to true)", Optional = true)]
+            public bool AllowPartialMapping;
+        }
+
+        static void Main(params string[] args)
+        {
+            var options = new Options();
+            var parser = new CommandLineParser.CommandLineParser();
+            parser.ExtractArgumentAttributes(options);
+            parser.ParseCommandLine(args);
+
+            try
+            {
+                parser.ParseCommandLine(args);
+
+                var server = FluentMockServer.StartWithAdminInterface(options.Urls);
+
+                if (options.AllowPartialMapping)
+                    server.AllowPartialMapping();
+
+                Console.WriteLine("WireMock.Net server listening at {0}", string.Join(" and ", server.Urls));
+            }
+            catch (CommandLineException e)
+            {
+                Console.WriteLine(e.Message);
+                parser.ShowUsage();
+            }
+  
+            Console.WriteLine("Press any key to stop the server");
+            Console.ReadKey();
+        }
+    }
+}
--- src/WireMock.Net.StandAlone/Properties/AssemblyInfo.cs
+++ src/WireMock.Net.StandAlone/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WireMock.Net.ConsoleApplication")]
+[assembly: AssemblyTitle("WireMock.Net.StandAlone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WireMock.Net.ConsoleApplication")]
+[assembly: AssemblyProduct("WireMock.Net.StandAlone")]
 [assembly: AssemblyCopyright("Copyright © Stef Heyenrath 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("668f689e-57b4-422e-8846-c0ff643ca268")]
+[assembly: Guid("668f689e-57b4-422e-8846-c0ff643ca999")]
 
 // Version information for an assembly consists of the following four values:
 //
