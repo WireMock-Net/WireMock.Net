3c22526905184c3943e7379c61a408500969a698
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-05-23 21:56:53 +0200

    Added some RestEase interfaces

---
+++ src/WireMock.Net.Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestEase;
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using WireMock.Client;
+
+namespace WireMock.Net.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an implementation of the IFluentMockServerAdmin and pass in the base URL for the API.
+            var api = RestClient.For<IFluentMockServerAdmin>("http://localhost:9090");
+
+            // Set BASIC Auth
+            var value = Convert.ToBase64String(Encoding.ASCII.GetBytes("a:b"));
+            api.Authorization = new AuthenticationHeaderValue("Basic", value);
+
+            var settings1 = api.GetSettingsAsync().Result;
+            Console.WriteLine($"settings1 = {JsonConvert.SerializeObject(settings1)}");
+
+            settings1.GlobalProcessingDelay = 1077;
+            api.PostSettingsAsync(settings1).Wait();
+
+            var settings2 = api.GetSettingsAsync().Result;
+            Console.WriteLine($"settings2 = {JsonConvert.SerializeObject(settings2)}");
+
+            var mappings = api.GetMappingsAsync().Result;
+            Console.WriteLine($"mappings = {JsonConvert.SerializeObject(mappings)}");
+
+            var guid = Guid.Parse("11111110-a633-40e8-a244-5cb80bc0ab66");
+            var mapping = api.GetMappingAsync(guid).Result;
+            Console.WriteLine($"mapping = {JsonConvert.SerializeObject(mapping)}");
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/WireMock.Net/Client/IFluentMockServerAdmin.cs
@@ -0,0 +1,51 @@
+﻿using RestEase;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using WireMock.Admin.Mappings;
+using WireMock.Admin.Settings;
+
+namespace WireMock.Client
+{
+    /// <summary>
+    /// The RestEase interface which defines all admin commands.
+    /// </summary>
+    public interface IFluentMockServerAdmin
+    {
+        /// <summary>
+        /// Authentication header
+        /// </summary>
+        [Header("Authorization")]
+        AuthenticationHeaderValue Authorization { get; set; }
+
+        /// <summary>
+        /// Get the settings.
+        /// </summary>
+        /// <returns>SettingsModel</returns>
+        [Get("__admin/settings")]
+        Task<SettingsModel> GetSettingsAsync();
+
+        /// <summary>
+        /// Post the settings
+        /// </summary>
+        /// <param name="settings">SettingsModel</param>
+        [Post("__admin/settings")]
+        Task PostSettingsAsync([Body] SettingsModel settings);
+
+        /// <summary>
+        /// Get the mappings.
+        /// </summary>
+        /// <returns>MappingModels</returns>
+        [Get("__admin/mappings")]
+        Task<IList<MappingModel>> GetMappingsAsync();
+
+        /// <summary>
+        /// Get a mappings based on the guid
+        /// </summary>
+        /// <param name="guid">The Guid</param>
+        /// <returns>MappingModel</returns>
+        [Get("__admin/mappings/{guid}")]
+        Task<MappingModel> GetMappingAsync([Path] Guid guid);
+    }
+}
