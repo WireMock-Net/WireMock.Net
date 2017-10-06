be5ff3c7d2d4b4edb6ee38b3e32fb2bc33990b8f
deeptowncitizen <reshetnikd@gmail.com>
2017-10-06 16:24:14 -0400

    event test

---
+++ test/WireMock.Net.Tests/ObservableLogEntriesTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using RestEase;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using Xunit;
+
+namespace WireMock.Net.Tests
+{
+    public class ObservableLogEntriesTest: IDisposable
+    {
+        private FluentMockServer _server;
+
+        [Fact]
+        public async void Test()
+        {
+            // given
+            _server = FluentMockServer.Start();
+
+            _server
+                .Given(Request.Create()
+                    .WithPath("/foo")
+                    .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithStatusCode(200)
+                    .WithBody(@"{ msg: ""Hello world!""}"));
+
+            var count = 0;
+            _server.LogEntriesChanged += (sender, args) => count++;
+
+            // when
+            var response = await new HttpClient().GetAsync("http://localhost:" + _server.Ports[0] + "/foo");
+
+            // then
+            Check.That(count).Equals(1);
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+        }
+    }
+}
