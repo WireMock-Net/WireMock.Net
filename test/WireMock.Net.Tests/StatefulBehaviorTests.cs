93dd317b97f316fd56a33b3e13763e9414d898ba
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-02-24 09:32:12 +0100

    fix tests

--- test/WireMock.Net.Tests/StatefulBehaviorTests.cs
+++ test/WireMock.Net.Tests/StatefulBehaviorTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using NFluent;
 using WireMock.RequestBuilders;
@@ -11,83 +10,85 @@ using Xunit;
 
 namespace WireMock.Net.Tests
 {
-    public class StatefulBehaviorTests : IDisposable
+    public class StatefulBehaviorTests
     {
-        private FluentMockServer _server;
-
         [Fact]
         public async Task Should_skip_non_relevant_states()
         {
             // given
-            _server = FluentMockServer.Start();
+            var server = FluentMockServer.Start();
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/foo").UsingGet())
                 .InScenario("s")
                 .WhenStateIs("Test state")
                 .RespondWith(Response.Create());
 
             // when
-            var response = await new HttpClient().GetAsync("http://localhost:" + _server.Ports[0] + "/foo");
+            var response = await new HttpClient().GetAsync("http://localhost:" + server.Ports[0] + "/foo");
 
             // then
             Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.NotFound);
+
+            server.Dispose();
         }
 
         [Fact]
         public async Task Should_process_request_if_equals_state_and_single_state_defined()
         {
             // given
-            _server = FluentMockServer.Start();
+            var server = FluentMockServer.Start();
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/foo").UsingGet())
                 .InScenario("s")
                 .WillSetStateTo("Test state")
                 .RespondWith(Response.Create().WithBody("No state msg"));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/foo").UsingGet())
                 .InScenario("s")
                 .WhenStateIs("Test state")
                 .RespondWith(Response.Create().WithBody("Test state msg"));
 
             // when
-            var responseNoState = await new HttpClient().GetStringAsync("http://localhost:" + _server.Ports[0] + "/foo");
-            var responseWithState = await new HttpClient().GetStringAsync("http://localhost:" + _server.Ports[0] + "/foo");
+            var responseNoState = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/foo");
+            var responseWithState = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/foo");
 
             // then
             Check.That(responseNoState).Equals("No state msg");
             Check.That(responseWithState).Equals("Test state msg");
+
+            server.Dispose();
         }
 
         [Fact]
         public async Task Scenario_and_State_TodoList_Example()
         {
             // Assign
-            _server = FluentMockServer.Start();
+            var server = FluentMockServer.Start();
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/todo/items").UsingGet())
                 .InScenario("To do list")
                 .WillSetStateTo("TodoList State Started")
                 .RespondWith(Response.Create().WithBody("Buy milk"));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/todo/items").UsingPost())
                 .InScenario("To do list")
                 .WhenStateIs("TodoList State Started")
                 .WillSetStateTo("Cancel newspaper item added")
                 .RespondWith(Response.Create().WithStatusCode(201));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/todo/items").UsingGet())
                 .InScenario("To do list")
                 .WhenStateIs("Cancel newspaper item added")
                 .RespondWith(Response.Create().WithBody("Buy milk;Cancel newspaper subscription"));
 
             // Act and Assert
-            string url = "http://localhost:" + _server.Ports[0];
+            string url = "http://localhost:" + server.Ports[0];
             string getResponse1 = new HttpClient().GetStringAsync(url + "/todo/items").Result;
             Check.That(getResponse1).Equals("Buy milk");
 
@@ -96,40 +97,42 @@ namespace WireMock.Net.Tests
 
             string getResponse2 = await new HttpClient().GetStringAsync(url + "/todo/items");
             Check.That(getResponse2).Equals("Buy milk;Cancel newspaper subscription");
+
+            server.Dispose();
         }
 
         [Fact]
         public async Task Should_process_request_if_equals_state_and_multiple_state_defined()
         {
             // Assign
-            _server = FluentMockServer.Start();
+            var server = FluentMockServer.Start();
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/state1").UsingGet())
                 .InScenario("s1")
                 .WillSetStateTo("Test state 1")
                 .RespondWith(Response.Create().WithBody("No state msg 1"));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/fooX").UsingGet())
                 .InScenario("s1")
                 .WhenStateIs("Test state 1")
                 .RespondWith(Response.Create().WithBody("Test state msg 1"));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/state2").UsingGet())
                 .InScenario("s2")
                 .WillSetStateTo("Test state 2")
                 .RespondWith(Response.Create().WithBody("No state msg 2"));
 
-            _server
+            server
                 .Given(Request.Create().WithPath("/fooX").UsingGet())
                 .InScenario("s2")
                 .WhenStateIs("Test state 2")
                 .RespondWith(Response.Create().WithBody("Test state msg 2"));
 
             // Act and Assert
-            string url = "http://localhost:" + _server.Ports[0];
+            string url = "http://localhost:" + server.Ports[0];
             var responseNoState1 = await new HttpClient().GetStringAsync(url + "/state1");
             Check.That(responseNoState1).Equals("No state msg 1");
 
@@ -141,11 +144,8 @@ namespace WireMock.Net.Tests
 
             var responseWithState2 = await new HttpClient().GetStringAsync(url + "/fooX");
             Check.That(responseWithState2).Equals("Test state msg 2");
-        }
 
-        public void Dispose()
-        {
-            _server?.Dispose();
+            server.Dispose();
         }
     }
 }
