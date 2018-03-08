3e634c2fdeb1946e4d28f1f9bb38d5cf57d0f682
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-03-08 18:28:53 +0100

    Example : WireMock.Net.StandAlone.Net461 (#104)

---
+++ examples/WireMock.Net.StandAlone.Net461/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace WireMock.Net.StandAlone.Net461
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = StandAloneApp.Start(args);
+            server.Given(Request.Create())
+                .RespondWith(Response.Create().WithProxy("http://10.10.66.65"));
+
+            Console.WriteLine("Press any key to stop the server");
+            Console.ReadKey();
+        }
+    }
+}
