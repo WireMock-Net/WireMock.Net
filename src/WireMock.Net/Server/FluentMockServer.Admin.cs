e405fae6610dd3e46cb22b402b694c6da871be26
Unknown <ba.raghu@gmail.com>
2018-04-10 12:39:42 +0530

    Fix for issue #118

--- src/WireMock.Net/Server/FluentMockServer.Admin.cs
+++ src/WireMock.Net/Server/FluentMockServer.Admin.cs
@@ -745,6 +745,11 @@ namespace WireMock.Server
                 responseBuilder = responseBuilder.WithBodyFromBase64(responseModel.BodyFromBase64, ToEncoding(responseModel.BodyEncoding));
             }
 
+            else if (responseModel.BodyAsFile != null)
+            {
+                responseBuilder = responseBuilder.WithBodyFromFile(responseModel.BodyAsFile);
+            }
+
             if (responseModel.UseTransformer)
             {
                 responseBuilder = responseBuilder.WithTransformer();
