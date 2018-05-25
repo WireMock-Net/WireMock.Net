e4ff8a61eba35cd6b3587850b73e58f4c74c2c3b
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-05-25 21:03:14 +0200

    1.0.3.18

--- test/WireMock.Net.Tests/FluentMockServerTests.Proxy.cs
+++ test/WireMock.Net.Tests/FluentMockServerTests.Proxy.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading;
 using System.Threading.Tasks;
 using NFluent;
-using WireMock.Matchers.Request;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 using WireMock.Server;
