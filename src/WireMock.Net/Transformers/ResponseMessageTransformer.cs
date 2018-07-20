f14468f693cc487f2ca0e8762f6af77e7129f93a
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-07-20 19:50:25 +0200

    Fix Sonar Issue

--- src/WireMock.Net/Transformers/ResponseMessageTransformer.cs
+++ src/WireMock.Net/Transformers/ResponseMessageTransformer.cs
@@ -67,57 +67,57 @@ namespace WireMock.Transformers
                     break;
             }
 
-            void WalkNode(JToken node)
-            {
-                if (node.Type == JTokenType.Object)
-                {
-                    // In case of Object, loop all children.
-                    foreach (JProperty child in node.Children<JProperty>())
-                    {
-                        WalkNode(child.Value);
-                    }
-                }
-                else if (node.Type == JTokenType.Array)
-                {
-                    // In case of Array, loop all items.
-                    foreach (JToken child in node.Children())
-                    {
-                        WalkNode(child);
-                    }
-                }
-                else if (node.Type == JTokenType.String)
-                {
-                    // In case of string, try to transform the value.
-                    string stringValue = node.Value<string>();
-                    if (string.IsNullOrEmpty(stringValue))
-                    {
-                        return;
-                    }
-
-                    var templateForStringValue = Handlebars.Compile(stringValue);
-                    string transformedString = templateForStringValue(template);
-                    if (!string.Equals(stringValue, transformedString))
-                    {
-                        JToken value;
-                        try
-                        {
-                            // Try to convert this string into a real JsonObject
-                            value = JToken.Parse(transformedString);
-                        }
-                        catch (JsonException)
-                        {
-                            // Ignore JsonException and just convert to JToken
-                            value = transformedString;
-                        }
-
-                        node.Replace(value);
-                    }
-                }
-            }
-
-            WalkNode(jobject);
+            WalkNode(jobject, template);
 
             responseMessage.BodyAsJson = jobject;
+        }
+
+        private static void WalkNode(JToken node, object template)
+        {
+            if (node.Type == JTokenType.Object)
+            {
+                // In case of Object, loop all children.
+                foreach (JProperty child in node.Children<JProperty>())
+                {
+                    WalkNode(child.Value, template);
+                }
+            }
+            else if (node.Type == JTokenType.Array)
+            {
+                // In case of Array, loop all items.
+                foreach (JToken child in node.Children())
+                {
+                    WalkNode(child, template);
+                }
+            }
+            else if (node.Type == JTokenType.String)
+            {
+                // In case of string, try to transform the value.
+                string stringValue = node.Value<string>();
+                if (string.IsNullOrEmpty(stringValue))
+                {
+                    return;
+                }
+
+                var templateForStringValue = Handlebars.Compile(stringValue);
+                string transformedString = templateForStringValue(template);
+                if (!string.Equals(stringValue, transformedString))
+                {
+                    JToken value;
+                    try
+                    {
+                        // Try to convert this string into a real JsonObject
+                        value = JToken.Parse(transformedString);
+                    }
+                    catch (JsonException)
+                    {
+                        // Ignore JsonException and just convert to JToken
+                        value = transformedString;
+                    }
+
+                    node.Replace(value);
+                }
+            }
         }
 
         private static void TransformBodyAsString(object template, ResponseMessage original, ResponseMessage responseMessage)
