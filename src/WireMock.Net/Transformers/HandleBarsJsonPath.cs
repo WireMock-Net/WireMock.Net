b3749e45a07fbe8a0ef2534f75831fe8d031306f
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-09-07 17:43:10 +0200

    #201

---
+++ src/WireMock.Net/Transformers/HandleBarsJsonPath.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HandlebarsDotNet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WireMock.Validation;
+
+namespace WireMock.Transformers
+{
+    internal static class HandleBarsJsonPath
+    {
+        public static void Register()
+        {
+            Handlebars.RegisterHelper("JsonPath.SelectToken", (writer, context, arguments) =>
+            {
+                (JObject valueToProcess, string jsonpath) = ParseArguments(arguments);
+
+                JToken result = null;
+                try
+                {
+                    result = valueToProcess.SelectToken(jsonpath);
+                }
+                catch (JsonException)
+                {
+                    // Ignore JsonException and return
+                    return;
+                }
+
+                if (result != null)
+                {
+                    writer.WriteSafeString(result);
+                }
+            });
+
+            Handlebars.RegisterHelper("JsonPath.SelectTokens", (writer, options, context, arguments) =>
+            {
+                (JObject valueToProcess, string jsonpath) = ParseArguments(arguments);
+
+                IEnumerable<JToken> values = null;
+                try
+                {
+                    values = valueToProcess.SelectTokens(jsonpath);
+                }
+                catch (JsonException)
+                {
+                    // Ignore JsonException and return
+                    return;
+                }
+
+                if (values == null)
+                {
+                    return;
+                }
+
+                options.Template(writer, values.ToDictionary(value => value.Path, value => value));
+            });
+        }
+
+        private static (JObject valueToProcess, string jsonpath) ParseArguments(object[] arguments)
+        {
+            Check.Condition(arguments, args => args.Length == 2, nameof(arguments));
+            Check.NotNull(arguments[0], "arguments[0]");
+            Check.NotNullOrEmpty(arguments[1] as string, "arguments[1]");
+
+            JObject valueToProcess;
+
+            switch (arguments[0])
+            {
+                case string jsonAsString:
+                    valueToProcess = JObject.Parse(jsonAsString);
+                    break;
+
+                case JObject jsonAsJObject:
+                    valueToProcess = jsonAsJObject;
+                    break;
+
+                default:
+                    throw new NotSupportedException($"The value '{arguments[0]}' with type '{arguments[0]?.GetType()}' cannot be used in Handlebars JsonPath.");
+            }
+
+            return (valueToProcess, arguments[1] as string);
+        }
+    }
+}
---
+++ test/WireMock.Net.Tests/ResponseBuilderTests/ResponseWithHandlebarsRegexTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using NFluent;
+using WireMock.Models;
+using WireMock.ResponseBuilders;
+using WireMock.Util;
+using Xunit;
+
+namespace WireMock.Net.Tests.ResponseBuilderTests
+{
+    public class ResponseWithHandlebarsRegexTests
+    {
+        private const string ClientIp = "::1";
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatch()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "abc" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{Regex.Match request.body \"^(?<x>\\w+)$\"}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("abc");
+        }
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatch_NoMatch()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "abc" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{Regex.Match request.body \"^?0$\"}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("");
+        }
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatch_NoMatch_WithDefaultValue()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "abc" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{Regex.Match request.body \"^?0$\" \"d\"}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("d");
+        }
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatches()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "https://localhost:5000/" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{#Regex.Matches request.body \"^(?<proto>\\w+)://[^/]+?(?<port>\\d+)/?\"}}{{this.port}}-{{this.proto}}{{/Regex.Matches}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("5000-https");
+        }
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatches_NoMatch()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "{{\\test" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{#Regex.Matches request.body \"^(?<proto>\\w+)://[^/]+?(?<port>\\d+)/?\"}}{{this}}{{/Regex.Matches}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("");
+        }
+
+        [Fact]
+        public async void Response_ProvideResponse_Handlebars_RegexMatches_NoMatch_WithDefaultValue()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "{{\\test" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{#Regex.Matches request.body \"^(?<proto>\\w+)://[^/]+?(?<port>\\d+)/?\" \"x\"}}{{this}}{{/Regex.Matches}}")
+                .WithTransformer();
+
+            // Act
+            var responseMessage = await response.ProvideResponseAsync(request);
+
+            // assert
+            Check.That(responseMessage.Body).Equals("x");
+        }
+
+        [Fact]
+        public void Response_ProvideResponse_Handlebars_RegexMatches_Throws()
+        {
+            // Assign
+            var body = new BodyData { BodyAsString = "{{\\test" };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var response = Response.Create()
+                .WithBody("{{#Regex.Matches request.bodyAsJson \"^(?<proto>\\w+)://[^/]+?(?<port>\\d+)/?\"}}{{/Regex.Matches}}")
+                .WithTransformer();
+
+            // Act and Assert
+            Check.ThatAsyncCode(() => response.ProvideResponseAsync(request)).Throws<NotSupportedException>();
+        }
+    }
+}
