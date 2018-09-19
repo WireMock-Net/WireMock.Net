6e4428114b6f5054016455fb9efd730acf438670
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-09-19 17:41:49 +0200

    more tests

---
+++ test/WireMock.Net.Tests/TestUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace WireMock.Net.Tests
+{
+    public static class TestUtils
+    {
+        public static T GetPrivateFieldValue<T>(this object obj, string fieldName)
+        {
+            var field = obj.GetType().GetTypeInfo().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return (T)field.GetValue(obj);
+        }
+    }
+}
