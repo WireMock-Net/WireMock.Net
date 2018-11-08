ee7f939e34f39bcbcf5fa31af3d74fa1decc676d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-08 11:33:20 +0000

    Update dependencies (#221)
    * update some dependencies
    
    * update deps

--- test/WireMock.Net.Tests/FluentMockServerTests.Proxy2.cs
+++ test/WireMock.Net.Tests/FluentMockServerTests.Proxy2.cs
@@ -17,15 +17,15 @@ namespace WireMock.Net.Tests
         public async Task FluentMockServer_ProxyAndRecordSettings_ShouldProxy()
         {
             // Assign
-            var server = FluentMockServer.Start();
-
-            server.Given(Request.Create().UsingPost().WithHeader("prx", "1"))
-                .RespondWith(Response.Create().WithProxy(server.Urls[0]));
-
-            server.Given(Request.Create().UsingPost())
+            var serverAsProxy = FluentMockServer.Start();
+            serverAsProxy.Given(Request.Create().UsingPost())
                 .RespondWith(Response.Create().WithStatusCode(201).WithBodyAsJson(new { p = 42 }).WithHeader("Content-Type", "application/json"));
 
             // Act
+            var server = FluentMockServer.Start();
+            server.Given(Request.Create().UsingPost().WithHeader("prx", "1"))
+                .RespondWith(Response.Create().WithProxy(serverAsProxy.Urls[0]));
+
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
@@ -41,6 +41,9 @@ namespace WireMock.Net.Tests
             Check.That(content).IsEqualTo("{\"p\":42}");
             Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.Created);
             Check.That(response.Content.Headers.GetValues("Content-Type").First()).IsEqualTo("application/json");
+
+            server.Dispose();
+            serverAsProxy.Dispose();
         }
     }
 }
