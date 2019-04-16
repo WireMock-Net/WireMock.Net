2a95f9569ebea7f0a15669248ea34914797aec3b
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-04-16 16:01:00 +0200

    JmesPathMatcher

---
+++ src/WireMock.Net/Matchers/JmesPathMatcher.cs
@@ -0,0 +1,98 @@
+﻿using DevLab.JmesPath;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using WireMock.Validation;
+
+namespace WireMock.Matchers
+{
+    /// <summary>
+    /// http://jmespath.org/
+    /// </summary>
+    public class JmesPathMatcher : IStringMatcher, IObjectMatcher
+    {
+        private readonly string[] _patterns;
+
+        /// <inheritdoc cref="IMatcher.MatchBehaviour"/>
+        public MatchBehaviour MatchBehaviour { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JmesPathMatcher"/> class.
+        /// </summary>
+        /// <param name="patterns">The patterns.</param>
+        public JmesPathMatcher([NotNull] params string[] patterns) : this(MatchBehaviour.AcceptOnMatch, patterns)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JmesPathMatcher"/> class.
+        /// </summary>
+        /// <param name="matchBehaviour">The match behaviour.</param>
+        /// <param name="patterns">The patterns.</param>
+        public JmesPathMatcher(MatchBehaviour matchBehaviour, [NotNull] params string[] patterns)
+        {
+            Check.NotNull(patterns, nameof(patterns));
+
+            MatchBehaviour = matchBehaviour;
+            _patterns = patterns;
+        }
+
+        /// <inheritdoc cref="IStringMatcher.IsMatch"/>
+        public double IsMatch(string input)
+        {
+            double match = MatchScores.Mismatch;
+            if (input != null)
+            {
+                try
+                {
+                    match = MatchScores.ToScore(_patterns.Select(pattern => bool.Parse(new JmesPath().Transform(input, pattern))));
+                }
+                catch (JsonException)
+                {
+                    // just ignore JsonException
+                }
+            }
+
+            return MatchBehaviourHelper.Convert(MatchBehaviour, match);
+        }
+
+        /// <inheritdoc cref="IObjectMatcher.IsMatch"/>
+        public double IsMatch(object input)
+        {
+            double match = MatchScores.Mismatch;
+
+            // When input is null or byte[], return Mismatch.
+            if (input != null && !(input is byte[]))
+            {
+                try
+                {
+                    // Check if JToken or object
+                    JToken jToken = input is JToken token ? token : JObject.FromObject(input);
+                    match = MatchScores.ToScore(_patterns.Select(pattern =>
+                    {
+                        var jmes = new JmesPath();
+                        string inputAsString = JsonConvert.SerializeObject(jToken);
+
+                        return bool.Parse(jmes.Transform(inputAsString, pattern));
+                    }));
+                }
+                catch (JsonException)
+                {
+                    // just ignore JsonException
+                }
+            }
+
+            return MatchBehaviourHelper.Convert(MatchBehaviour, match);
+        }
+
+        /// <inheritdoc cref="IStringMatcher.GetPatterns"/>
+        public string[] GetPatterns()
+        {
+            return _patterns;
+        }
+
+        /// <inheritdoc cref="IMatcher.Name"/>
+        public string Name => "JmesPathMatcher";
+    }
+}
