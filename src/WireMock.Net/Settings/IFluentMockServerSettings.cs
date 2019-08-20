e616704921bb6e9f7d29e230644476972ca775e0
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-08-20 21:51:14 +0200

    ISettings

--- src/WireMock.Net/Settings/IFluentMockServerSettings.cs
+++ src/WireMock.Net/Settings/IFluentMockServerSettings.cs
@@ -15,80 +15,80 @@ namespace WireMock.Settings
         /// Gets or sets the port.
         /// </summary>
         [PublicAPI]
-        int? Port { get; set; }
+        int? Port { get; }
 
         /// <summary>
         /// Gets or sets the use SSL.
         /// </summary>
         // ReSharper disable once InconsistentNaming
         [PublicAPI]
-        bool? UseSSL { get; set; }
+        bool? UseSSL { get; }
 
         /// <summary>
         /// Gets or sets whether to start admin interface.
         /// </summary>
         [PublicAPI]
-        bool? StartAdminInterface { get; set; }
+        bool? StartAdminInterface { get; }
 
         /// <summary>
         /// Gets or sets if the static mappings should be read at startup.
         /// </summary>
         [PublicAPI]
-        bool? ReadStaticMappings { get; set; }
+        bool? ReadStaticMappings { get; }
 
         /// <summary>
         /// Watch the static mapping files + folder for changes when running.
         /// </summary>
         [PublicAPI]
-        bool? WatchStaticMappings { get; set; }
+        bool? WatchStaticMappings { get; }
 
         /// <summary>
         /// Gets or sets if the proxy and record settings.
         /// </summary>
         [PublicAPI]
-        IProxyAndRecordSettings ProxyAndRecordSettings { get; set; }
+        IProxyAndRecordSettings ProxyAndRecordSettings { get; }
 
         /// <summary>
         /// Gets or sets the urls.
         /// </summary>
         [PublicAPI]
-        string[] Urls { get; set; }
+        string[] Urls { get; }
 
         /// <summary>
         /// StartTimeout
         /// </summary>
         [PublicAPI]
-        int StartTimeout { get; set; }
+        int StartTimeout { get; }
 
         /// <summary>
         /// Allow Partial Mapping (default set to false).
         /// </summary>
         [PublicAPI]
-        bool? AllowPartialMapping { get; set; }
+        bool? AllowPartialMapping { get; }
 
         /// <summary>
         /// The username needed for __admin access.
         /// </summary>
         [PublicAPI]
-        string AdminUsername { get; set; }
+        string AdminUsername { get; }
 
         /// <summary>
         /// The password needed for __admin access.
         /// </summary>
         [PublicAPI]
-        string AdminPassword { get; set; }
+        string AdminPassword { get; }
 
         /// <summary>
         /// The RequestLog expiration in hours (optional).
         /// </summary>
         [PublicAPI]
-        int? RequestLogExpirationDuration { get; set; }
+        int? RequestLogExpirationDuration { get; }
 
         /// <summary>
         /// The MaxRequestLog count (optional).
         /// </summary>
         [PublicAPI]
-        int? MaxRequestLogCount { get; set; }
+        int? MaxRequestLogCount { get; }
 
         /// <summary>
         /// Action which is called (with the IAppBuilder or IApplicationBuilder) before the internal WireMockMiddleware is initialized. [Optional]
@@ -118,6 +118,6 @@ namespace WireMock.Settings
         /// Action which can be used to add additional is Handlebar registrations. [Optional]
         /// </summary>
         [PublicAPI]
-        Action<IHandlebars, IFileSystemHandler> HandlebarsRegistrationCallback { get; set; }
+        Action<IHandlebars, IFileSystemHandler> HandlebarsRegistrationCallback { get;  }
     }
 }
