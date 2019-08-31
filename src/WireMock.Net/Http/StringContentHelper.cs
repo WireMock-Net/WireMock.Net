c0a43ed20475778ca0661a18c3e8d00305382c54
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-08-31 19:01:44 +0000

    remove MimeKitLite and use MediaTypeHeaderValue (#338)

--- src/WireMock.Net/Http/StringContentHelper.cs
+++ src/WireMock.Net/Http/StringContentHelper.cs
@@ -1,8 +1,6 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using JetBrains.Annotations;
-using MimeKit;
 using WireMock.Validation;
 
 namespace WireMock.Http
@@ -10,29 +8,18 @@ namespace WireMock.Http
     internal static class StringContentHelper
     {
         /// <summary>
-        /// Creates a StringContent object. Note that the Encoding is only set when it's also set on the original header.
+        /// Creates a StringContent object.
         /// </summary>
         /// <param name="content">The string content (cannot be null)</param>
         /// <param name="contentType">The ContentType (can be null)</param>
         /// <returns>StringContent</returns>
-        internal static StringContent Create([NotNull] string content, [CanBeNull] ContentType contentType)
+        internal static StringContent Create([NotNull] string content, [CanBeNull] MediaTypeHeaderValue contentType)
         {
             Check.NotNull(content, nameof(content));
 
-            if (contentType == null)
-            {
-                return new StringContent(content);
-            }
-
-            if (contentType.Charset == null)
-            {
-                var stringContent = new StringContent(content);
-                stringContent.Headers.ContentType = new MediaTypeHeaderValue(contentType.MimeType);
-                return stringContent;
-            }
-
-            var encoding = Encoding.GetEncoding(contentType.Charset);
-            return new StringContent(content, encoding, contentType.MimeType);
+            var stringContent = new StringContent(content);
+            stringContent.Headers.ContentType = contentType;
+            return stringContent;
         }
     }
 }
---
+++ test/WireMock.Net.Tests/Http/StringContentHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using FluentAssertions;
+using WireMock.Http;
+using Xunit;
+
+namespace WireMock.Net.Tests.Http
+{
+    public class StringContentHelperTests
+    {
+        [Fact]
+        public void StringContentHelper_Create_WithNullContentType()
+        {
+            // Act
+            var result = StringContentHelper.Create("test", null);
+
+            // Assert
+            result.Headers.ContentType.Should().BeNull();
+            result.ReadAsStringAsync().Result.Should().Be("test");
+        }
+
+        [Theory]
+        [InlineData("application/json", "application/json")]
+        [InlineData("application/soap+xml", "application/soap+xml")]
+        [InlineData("application/soap+xml;charset=UTF-8", "application/soap+xml; charset=UTF-8")]
+        [InlineData("application/soap+xml;charset=UTF-8;action=\"http://myCompany.Customer.Contract/ICustomerService/GetSomeConfiguration\"", "application/soap+xml; charset=UTF-8; action=\"http://myCompany.Customer.Contract/ICustomerService/GetSomeConfiguration\"")]
+        public void StringContentHelper_Create(string test, string expected)
+        {
+            // Arrange
+            var contentType = MediaTypeHeaderValue.Parse(test);
+
+            // Act
+            var result = StringContentHelper.Create("test", contentType);
+
+            // Assert
+            result.Headers.ContentType.ToString().Should().Be(expected);
+            result.ReadAsStringAsync().Result.Should().Be("test");
+        }
+    }
+}
