e8a83147953afdaba73c1bb5b3d8057595e4d10d
Noah Lerner <noahlerner94@gmail.com>
2020-01-30 21:54:12 +0200

    Unit tests for relative path feature

--- test/WireMock.Net.Tests/ResponseBuilders/ResponseWithBodyFromFileTests.cs
+++ test/WireMock.Net.Tests/ResponseBuilders/ResponseWithBodyFromFileTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -44,6 +45,96 @@ namespace WireMock.Net.Tests.ResponseBuilders
             response1.Should().Contain("<hello>world</hello>");
             response2.Should().Contain("<hello>world</hello>");
             response3.Should().Contain("<hello>world</hello>");
+        }
+
+        [Fact]
+        public async Task Response_ProvideResponse_WithBodyFromFile_InSubDirectory()
+        {
+            // Arrange
+            var server = WireMockServer.Start();
+            int path = 0;
+            List<string> pathList = new List<string>()
+            {
+                @"subdirectory/MyXmlResponse.xml",
+                @"subdirectory\MyXmlResponse.xml",
+                @"/subdirectory/MyXmlResponse.xml",
+                @"\subdirectory\MyXmlResponse.xml"
+            };
+
+            server
+                .Given(
+                    Request
+                        .Create()
+                        .UsingGet()
+                        .WithPath("/v1/content/path1")
+                )
+                .RespondWith(
+                    Response
+                        .Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/xml")
+                        .WithBodyFromFile(pathList[path])
+                );
+
+            path++;
+            server
+                .Given(
+                    Request
+                        .Create()
+                        .UsingGet()
+                        .WithPath("/v1/content/path2")
+                )
+                .RespondWith(
+                    Response
+                        .Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/xml")
+                        .WithBodyFromFile(pathList[path])
+                );
+
+            path++;
+            server
+                .Given(
+                    Request
+                        .Create()
+                        .UsingGet()
+                        .WithPath("/v1/content/path3")
+                )
+                .RespondWith(
+                    Response
+                        .Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/xml")
+                        .WithBodyFromFile(pathList[path])
+                );
+
+            path++;
+            server
+                .Given(
+                    Request
+                        .Create()
+                        .UsingGet()
+                        .WithPath("/v1/content/path4")
+                )
+                .RespondWith(
+                    Response
+                        .Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/xml")
+                        .WithBodyFromFile(pathList[path])
+                );
+
+            // Act
+            var response1 = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/v1/content/path1");
+            var response2 = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/v1/content/path2");
+            var response3 = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/v1/content/path3");
+            var response4 = await new HttpClient().GetStringAsync("http://localhost:" + server.Ports[0] + "/v1/content/path4");
+
+            // Assert
+            response1.Should().Contain("<hello>world</hello>");
+            response2.Should().Contain("<hello>world</hello>");
+            response3.Should().Contain("<hello>world</hello>");
+            response4.Should().Contain("<hello>world</hello>");
         }
     }
 }
