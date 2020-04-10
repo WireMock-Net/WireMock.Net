372ba87531c9bf7490cf0063c2e0b69cb9db4860
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-04-10 20:42:33 +0200

    usings

--- src/WireMock.Net/Util/BodyParser.cs
+++ src/WireMock.Net/Util/BodyParser.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
