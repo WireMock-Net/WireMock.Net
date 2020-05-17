7d873611eee3e1f38aea5e67292c7f3772856d5c
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2020-05-17 19:55:36 +0200

    Limits (#457)

---
+++ src/WireMock.Net/Owin/AspNetCoreSelfHost.NETStandard.cs
@@ -0,0 +1,40 @@
+﻿#if USE_ASPNETCORE && !NETSTANDARD1_3
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace WireMock.Owin
+{
+    internal partial class AspNetCoreSelfHost
+    {
+        private static void SetKestrelOptionsLimits(KestrelServerOptions options)
+        {
+            options.Limits.KeepAliveTimeout = TimeSpan.MaxValue;
+            options.Limits.MaxRequestBodySize = null; // https://stackoverflow.com/questions/46738364/increase-upload-request-length-limit-in-kestrel
+            options.Limits.MaxRequestBufferSize = null;
+            options.Limits.MaxRequestHeaderCount = 100;
+            options.Limits.MaxResponseBufferSize = null;
+            options.Limits.RequestHeadersTimeout = TimeSpan.MaxValue;
+        }
+
+        private static void SetHttpsAndUrls(KestrelServerOptions options, ICollection<(string Url, int Port)> urlDetails)
+        {
+            foreach (var detail in urlDetails)
+            {
+                if (detail.Url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+                {
+                    options.Listen(System.Net.IPAddress.Any, detail.Port, listenOptions =>
+                    {
+                        listenOptions.UseHttps();
+                    });
+                }
+                else
+                {
+                    options.Listen(System.Net.IPAddress.Any, detail.Port);
+                }
+            }
+        }
+    }
+}
+#endif
