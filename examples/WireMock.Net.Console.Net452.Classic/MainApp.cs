de9fe75d9de399fab7da4975f721df114bd5693e
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-02 23:29:27 +0200

    update example - {{Math.Add 1 2}}

--- examples/WireMock.Net.Console.Net452.Classic/MainApp.cs
+++ examples/WireMock.Net.Console.Net452.Classic/MainApp.cs
@@ -328,7 +328,7 @@ namespace WireMock.Net.ConsoleApplication
                 .Given(Request.Create().WithPath("/partial").UsingPost().WithBody(new SimMetricsMatcher(new[] { "cat", "dog" })))
                 .RespondWith(Response.Create().WithStatusCode(200).WithBody("partial = 200"));
 
-            // http://localhost:8080/trans?start=1000&stop=1&stop=2
+            // http://localhost:9091/trans?start=1000&stop=1&stop=2
             server
                 .Given(Request.Create().WithPath("/trans").UsingGet())
                 .WithGuid("90356dba-b36c-469a-a17e-669cd84f1f05")
@@ -337,7 +337,7 @@ namespace WireMock.Net.ConsoleApplication
                     .WithHeader("Content-Type", "application/json")
                     .WithHeader("Transformed-Postman-Token", "token is {{request.headers.Postman-Token}}")
                     .WithHeader("xyz_{{request.headers.Postman-Token}}", "token is {{request.headers.Postman-Token}}")
-                    .WithBody(@"{""msg"": ""Hello world CATCH-ALL on /*, {{request.path}}, bykey={{request.query.start}}, bykey={{request.query.stop}}, byidx0={{request.query.stop.[0]}}, byidx1={{request.query.stop.[1]}}"" }")
+                    .WithBody(@"{""msg"": ""Hello world CATCH-ALL on /*, {{request.path}}, add={{Math.Add 1 2}} bykey={{request.query.start}}, bykey={{request.query.stop}}, byidx0={{request.query.stop.[0]}}, byidx1={{request.query.stop.[1]}}"" }")
                     .WithTransformer()
                     .WithDelay(TimeSpan.FromMilliseconds(100))
                 );
