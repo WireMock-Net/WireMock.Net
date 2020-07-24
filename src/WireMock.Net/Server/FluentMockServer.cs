8825e2fba1f7ba7d7513bc10045e11906f57487c
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-24 07:32:56 +0000

    FluentMockServerSettings

--- src/WireMock.Net/Server/FluentMockServer.cs
+++ src/WireMock.Net/Server/FluentMockServer.cs
@@ -1,7 +1,9 @@
-﻿using WireMock.Settings;
+﻿using System;
+using WireMock.Settings;
 
 namespace WireMock.Server
 {
+    [Obsolete("Use WireMockServer. This will removed in next version (1.3.x)")]
     public class FluentMockServer : WireMockServer
     {
         public FluentMockServer(IFluentMockServerSettings settings) : base((IWireMockServerSettings) settings)
