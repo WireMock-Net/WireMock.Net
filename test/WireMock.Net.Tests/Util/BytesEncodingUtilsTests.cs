f0bd9533ae05d06ffd1664449b8aba23150fe411
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-11-29 09:31:15 +0100

    BytesEncodingUtilsTests

---
+++ test/WireMock.Net.Tests/Util/BytesEncodingUtilsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using FluentAssertions;
+using WireMock.Util;
+using Xunit;
+
+namespace WireMock.Net.Tests.Util
+{
+    public class BytesEncodingUtilsTests
+    {
+        [Fact]
+        public void TryGetEncoding_InvalidBytes()
+        {
+            var result = BytesEncodingUtils.TryGetEncoding(new byte[] {0, 1}, out Encoding encoding);
+
+            // Assert
+            result.Should().BeFalse();
+            encoding.Should().BeNull();
+        }
+    }
+}
