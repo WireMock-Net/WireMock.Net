3deb393e54b4961f1f3c61a97d66cbcf74168cdd
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-12-03 12:35:07 +0000

    wip

---
+++ src/WireMock.Net/Plugin/PluginLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WireMock.Plugin
+{
+    internal static class PluginLoader
+    {
+        private static ConcurrentDictionary<Type, object> Assemblies = new ConcurrentDictionary<Type, object>();
+
+        public static T Load<T>(params object[] args) where T : class
+        {
+#if NETSTANDARD1_3
+            throw new NotSupportedException();
+#else
+            return Assemblies.GetOrAdd(typeof(T), (type) =>
+            {
+                var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll");
+
+                Type pluginType = null;
+                foreach (var file in files)
+                {
+                    var assembly = Assembly.LoadFile(Path.Combine(Directory.GetCurrentDirectory(), file));
+
+                    pluginType = assembly.GetTypes()
+                        .Where(t => typeof(T).IsAssignableFrom(t) && !t.GetTypeInfo().IsInterface)
+                        .FirstOrDefault();
+                    if (pluginType != null)
+                    {
+                        break;
+                    }
+                }
+
+                if (pluginType != null)
+                {
+                    return Activator.CreateInstance(pluginType, args);
+                }
+
+                throw new DllNotFoundException($"No dll found which implements type '{type}'");
+
+            }) as T;
+#endif
+        }
+    }
+}
