3b0dc467710e0d3a536f5cc4d7de1336d039c80d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-02-06 11:54:39 +0100

    Add Xamarin UI tests (#582)
    * xam
    
    * .
    
    * .

---
+++ test/WireMock.Net.XamarinUI.Tests/UIFlowTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+using Xamarin.UITest;
+
+namespace WireMock.Net.XamarinUI.Tests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class UIFlowTests
+    {
+        IApp app;
+        Platform platform;
+
+        private WireMockServer _server;
+
+        public UIFlowTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            var serverSettings = new WireMockServerSettings();
+            // serverSettings.Port = 5005;
+            serverSettings.AllowPartialMapping = true;
+
+            _server = WireMockServer.Start(serverSettings);
+        }
+
+        [Test]
+        public async Task TestMockServerIsWorking()
+        {
+            _server
+                .Given(Request.Create()
+                    .UsingGet())
+                .RespondWith(Response.Create()
+                    .WithBody("test"));
+
+            var httpClient = new HttpClient();
+
+            var result = await httpClient.GetAsync(_server.Urls[0]);
+            var content = await result.Content.ReadAsStringAsync();
+            Assert.AreEqual("test", content);
+        }
+
+        [TearDown]
+        public void ShutdownServer()
+        {
+            _server.Stop();
+        }
+    }
+}
