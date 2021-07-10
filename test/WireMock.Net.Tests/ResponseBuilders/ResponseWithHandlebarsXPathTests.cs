d91d37fccff671f8b8cbcc92b7e61014ec34894b
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-07-10 07:10:25 +0000

    xpath soap

--- test/WireMock.Net.Tests/ResponseBuilders/ResponseWithHandlebarsXPathTests.cs
+++ test/WireMock.Net.Tests/ResponseBuilders/ResponseWithHandlebarsXPathTests.cs
@@ -120,6 +120,65 @@ namespace WireMock.Net.Tests.ResponseBuilders
         }
 
         [Fact]
+        public async Task Response_ProvideResponse_Handlebars_XPath_SelectNodes_Request_SoapXML_BodyAsString()
+        {
+            // Assign
+            //<?xml version='1.0' standalone='no'?>
+            string soap = @"
+<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:ns=""http://www.Test.nl/XMLHeader/10"" xmlns:req=""http://www.Test.nl/Betalen/COU/Services/RdplDbTknLystByOvkLyst/8/Req"">
+   <soapenv:Header>
+      <ns:TestHeader>
+         <ns:HeaderVersion>10</ns:HeaderVersion>
+         <ns:MessageId>MsgId10</ns:MessageId>
+         <ns:ServiceRequestorDomain>Betalen</ns:ServiceRequestorDomain>
+         <ns:ServiceRequestorId>CRM</ns:ServiceRequestorId>
+         <ns:ServiceProviderDomain>COU</ns:ServiceProviderDomain>
+         <ns:ServiceId>RdplDbTknLystByOvkLyst</ns:ServiceId>
+         <ns:ServiceVersion>8</ns:ServiceVersion>
+         <ns:FaultIndication>N</ns:FaultIndication>
+         <ns:MessageTimestamp>?</ns:MessageTimestamp>
+      </ns:TestHeader>
+   </soapenv:Header>
+   <soapenv:Body>
+      <req:RdplDbTknLystByOvkLyst_REQ>
+         <req:AanleveraarCode>CRM</req:AanleveraarCode>
+         <!--Optional:-->
+         <req:AanleveraarDetail>CRMi</req:AanleveraarDetail>
+         <req:BerichtId>BerId</req:BerichtId>
+         <req:BerichtType>RdplDbTknLystByOvkLyst</req:BerichtType>
+         <!--Optional:-->
+         <req:OpgenomenBedragenGewenstIndicatie>N</req:OpgenomenBedragenGewenstIndicatie>
+         <req:TokenIdLijst>
+            <!--1 to 10 repetitions:-->
+            <req:TokenId>0000083256</req:TokenId>
+            <req:TokenId>0000083259</req:TokenId>
+         </req:TokenIdLijst>
+      </req:RdplDbTknLystByOvkLyst_REQ>
+   </soapenv:Body>
+</soapenv:Envelope>";
+            var body = new BodyData
+            {
+                BodyAsString = soap,
+                DetectedBodyType = BodyType.String
+            };
+
+            var request = new RequestMessage(new UrlDetails("http://localhost:1234"), "POST", ClientIp, body);
+
+            var responseBuilder = Response.Create()
+                .WithHeader("Content-Type", "application/xml")
+                .WithBody("<response>{{XPath.SelectNodes request.body \"//*[local-name()='TokenIdLijst']\"}}</response>")
+                .WithTransformer();
+
+            // Act
+            var response = await responseBuilder.ProvideResponseAsync(request, _settings);
+
+            // Assert
+            var nav = new XmlDocument { InnerXml = response.Message.BodyData.BodyAsString }.CreateNavigator();
+            var nodes = nav.XPath2SelectNodes("/response/todo-item");
+            Check.That(nodes.Count + 1).IsEqualTo(3);
+        }
+
+        [Fact]
         public async Task Response_ProvideResponse_Handlebars_XPath_Evaluate_Request_BodyAsString()
         {
             // Assign
