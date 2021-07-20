3982993810fba436a9c8b5bc83000c2ac4003367
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-07-20 09:24:18 +0200

    1.4.19-preview-01

--- src/WireMock.Net.Abstractions/bb.cs
+++ src/WireMock.Net.Abstractions/bb.cs
@@ -10,35 +10,35 @@ namespace FluentBuilder
 
         private Lazy<WireMock.Admin.Mappings.MatcherModel> _matcher = new Lazy<WireMock.Admin.Mappings.MatcherModel>(() => default(WireMock.Admin.Mappings.MatcherModel));
 
-        public BodyModelBuilder WithMatcher(WireMock.Admin.Mappings.MatcherModel value) => WithMatcher(() => value);
+        public BodyModelBuilder2 WithMatcher(WireMock.Admin.Mappings.MatcherModel value) => WithMatcher(() => value);
 
-        public BodyModelBuilder WithMatcher(Func<WireMock.Admin.Mappings.MatcherModel> func)
+        public BodyModelBuilder2 WithMatcher(Func<WireMock.Admin.Mappings.MatcherModel> func)
         {
             _matcher = new Lazy<WireMock.Admin.Mappings.MatcherModel>(func);
 
             return this;
         }
 
-        public BodyModelBuilder WithoutMatcher() => WithMatcher(() => default(WireMock.Admin.Mappings.MatcherModel));
+        public BodyModelBuilder2 WithoutMatcher() => WithMatcher(() => default(WireMock.Admin.Mappings.MatcherModel));
 
-        private Lazy<WireMock.Admin.Mappings.MatcherModel[]> _matchers = new Lazy<WireMock.Admin.Mappings.MatcherModel[]>(default(WireMock.Admin.Mappings.MatcherModel[]));
+        private Lazy<WireMock.Admin.Mappings.MatcherModel[]> _matchers = new Lazy<WireMock.Admin.Mappings.MatcherModel[]>(() => default(WireMock.Admin.Mappings.MatcherModel[]));
 
-        public BodyModelBuilder WithMatchers(WireMock.Admin.Mappings.MatcherModel[] value) => WithMatchers(() => value);
+        public BodyModelBuilder2 WithMatchers(WireMock.Admin.Mappings.MatcherModel[] value) => WithMatchers(() => value);
 
-        public BodyModelBuilder WithMatchers(Func<WireMock.Admin.Mappings.MatcherModel[]> func)
+        public BodyModelBuilder2 WithMatchers(Func<WireMock.Admin.Mappings.MatcherModel[]> func)
         {
             _matchers = new Lazy<WireMock.Admin.Mappings.MatcherModel[]>(func);
 
             return this;
         }
 
-        public BodyModelBuilder WithoutMatchers() => WithMatchers(() => default(WireMock.Admin.Mappings.MatcherModel[]));
+        public BodyModelBuilder2 WithoutMatchers() => WithMatchers(() => default(WireMock.Admin.Mappings.MatcherModel[]));
 
         public override BodyModel Build()
         {
             if (Object?.IsValueCreated != true)
             {
-                Object = new Lazy<BodyModel>(new BodyModel
+                Object = new Lazy<BodyModel>(() => new BodyModel
                 {
                     Matcher = _matcher.Value,
                     Matchers = _matchers.Value,
