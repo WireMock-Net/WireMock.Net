2bad136b9b8dfe24bb8a61e3af78248cf9709243
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-09-12 16:50:29 +0200

    usings

--- src/WireMock.Org.Abstractions/GetAdminMappingsResponse.cs
+++ src/WireMock.Org.Abstractions/GetAdminMappingsResponse.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace WireMock.Org.Abstractions
 {
     public class GetAdminMappingsResponse
--- src/WireMock.Org.RestClient/IWireMockOrgApi.cs
+++ src/WireMock.Org.RestClient/IWireMockOrgApi.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using RestEase;
 using WireMock.Org.Abstractions;
