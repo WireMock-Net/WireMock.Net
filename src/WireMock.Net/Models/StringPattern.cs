1bf523d3481841bb71e7ba3fc539636f9f432708
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-10-11 21:37:24 +0200

    StringPattern

--- src/WireMock.Net/Models/StringPattern.cs
+++ src/WireMock.Net/Models/StringPattern.cs
@@ -1,9 +1,18 @@
 namespace WireMock.Models
 {
+    /// <summary>
+    /// StringPattern which defines the Pattern as a string, and optionally the filepath pattern file.
+    /// </summary>
     public struct StringPattern
     {
+        /// <summary>
+        /// The pattern as string.
+        /// </summary>
         public string Pattern { get; set; }
 
+        /// <summary>
+        /// The filepath (optionally)
+        /// </summary>
         public string PatternAsFile { get; set; }
     }
 }
