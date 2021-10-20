22a31efc046d6086aeaa752ea4a8f1f25a4cd60f
Daniel <raiga1234@gmail.com>
2021-10-20 11:34:40 -0500

    Same to master

--- src/WireMock.Net.OpenApiParser/Mappers/OpenApiPathsMapper.cs
+++ src/WireMock.Net.OpenApiParser/Mappers/OpenApiPathsMapper.cs
@@ -270,7 +270,7 @@ namespace WireMock.Net.OpenApiParser.Mappers
                     }
                 })
                 .ToList();
-                
+
             return list.Any() ? list : null;
         }
 
