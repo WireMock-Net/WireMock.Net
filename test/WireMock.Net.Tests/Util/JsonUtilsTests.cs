b0f17f010355de8091594021c895ced4095e139d
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-02-21 22:38:54 +0100

    ...

--- test/WireMock.Net.Tests/Util/JsonUtilsTests.cs
+++ test/WireMock.Net.Tests/Util/JsonUtilsTests.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
-using Newtonsoft.Json.Linq;
-using NFluent;
 using System.Linq.Dynamic.Core;
 using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NFluent;
 using WireMock.Util;
 using Xunit;
 
@@ -41,40 +41,25 @@ namespace WireMock.Net.Tests.Util
             Check.That(line).IsEqualTo("string(it)");
         }
 
-        [Fact(Skip = "Bug in System.Linq.Dynamic.Core ?")]
-        public void JsonUtils_GenerateDynamicLinqStatement_JObject1()
+        [Fact]
+        public void JsonUtils_GenerateDynamicLinqStatement_JArray_Indexer()
         {
             // Assign
             var j = new JObject
             {
-                {"U", new JValue(new Uri("http://localhost:80/abc?a=5"))},
-                {"N", new JValue((object) null)},
-                {"G", new JValue(Guid.NewGuid())},
-                {"Flt", new JValue(10.0f)},
-                {"Dbl", new JValue(Math.PI)},
-                {"Check", new JValue(true)},
-                {"Items", new JArray(new[] { new JValue(4), new JValue(8) })}, // This fails
-                {
-                    "Child", new JObject
-                    {
-                        {"ChildId", new JValue(4)},
-                        {"ChildDateTime", new JValue(new DateTime(2018, 2, 17))},
-                        {"TS", new JValue(TimeSpan.FromMilliseconds(999))}
-                    }
-                },
-                {"I", new JValue(9)},
-                {"L", new JValue(long.MaxValue)},
-                {"Name", new JValue("Test")}
+                { "Items", new JArray(new[] { new JValue(4), new JValue(8) }) }
             };
 
             // Act
             string line = JsonUtils.GenerateDynamicLinqStatement(j);
-            line.Should().Be("new (Uri(U) as U, null as N, Guid(G) as G, double(Flt) as Flt, double(Dbl) as Dbl, bool(Check) as Check, (new [] { long(Items[0]), long(Items[1])}) as Items, new (long(Child.ChildId) as ChildId, DateTime(Child.ChildDateTime) as ChildDateTime, TimeSpan(Child.TS) as TS) as Child, long(I) as I, long(L) as L, string(Name) as Name)");
 
-            // Assert
+            // Assert 1
+            line.Should().Be("(new [] { long(Items[0]), long(Items[1])}) as Items)");
+
+            // Assert 2
             var queryable = new[] { j }.AsQueryable().Select(line);
-            //   bool result = queryable.Any("int(I) > 1 && L > 1");
-            //  Check.That(result).IsTrue();
+            bool result = queryable.Any("Items != null");
+            result.Should().BeTrue();
         }
 
         [Fact]
@@ -104,9 +89,11 @@ namespace WireMock.Net.Tests.Util
 
             // Act
             string line = JsonUtils.GenerateDynamicLinqStatement(j);
+
+            // Assert 1
             line.Should().Be("new (Uri(U) as U, null as N, Guid(G) as G, double(Flt) as Flt, double(Dbl) as Dbl, bool(Check) as Check, new (long(Child.ChildId) as ChildId, DateTime(Child.ChildDateTime) as ChildDateTime, TimeSpan(Child.TS) as TS) as Child, long(I) as I, long(L) as L, string(Name) as Name)");
 
-            // Assert
+            // Assert 2
             var queryable = new[] { j }.AsQueryable().Select(line);
             bool result = queryable.Any("I > 1 && L > 1");
             result.Should().BeTrue();
