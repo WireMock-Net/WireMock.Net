3871cf727949db744aaa07cc7e479a25f6b93f9c
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-05-18 19:27:33 +0200

    .

--- test/WireMock.Net.Tests/RequestMatchers/RequestMessageMethodMatcherTests.cs
+++ test/WireMock.Net.Tests/RequestMatchers/RequestMessageMethodMatcherTests.cs
@@ -12,7 +12,7 @@ public class RequestMessageMethodMatcherTests
     [InlineData("get", 1d)]
     [InlineData("post", 1d)]
     [InlineData("trace", 0d)]
-    public void RequestMessageMethodMatcherTests_GetMatchingScore(string method, double expected)
+    public void RequestMessageMethodMatcherTests_GetMatchingScore_Or(string method, double expected)
     {
         // Assign
         var requestMessage = new RequestMessage(new UrlDetails("http://localhost?key=test1"), method, "127.0.0.1");
@@ -24,5 +24,38 @@ public class RequestMessageMethodMatcherTests
 
         // Assert
         score.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("get", 0.5d)]
+    [InlineData("post", 0.5d)]
+    [InlineData("trace", 0d)]
+    public void RequestMessageMethodMatcherTests_GetMatchingScore_Average(string method, double expected)
+    {
+        // Assign
+        var requestMessage = new RequestMessage(new UrlDetails("http://localhost?key=test1"), method, "127.0.0.1");
+        var matcher = new RequestMessageMethodMatcher(MatchBehaviour.AcceptOnMatch, MatchOperator.Average, "Get", "Post");
+
+        // Act
+        var result = new RequestMatchResult();
+        double score = matcher.GetMatchingScore(requestMessage, result);
+
+        // Assert
+        score.Should().Be(expected);
+    }
+
+    [Fact]
+    public void RequestMessageMethodMatcherTests_GetMatchingScore_And()
+    {
+        // Assign
+        var requestMessage = new RequestMessage(new UrlDetails("http://localhost?key=test1"), "get", "127.0.0.1");
+        var matcher = new RequestMessageMethodMatcher(MatchBehaviour.AcceptOnMatch, MatchOperator.And, "Get", "Post");
+
+        // Act
+        var result = new RequestMatchResult();
+        double score = matcher.GetMatchingScore(requestMessage, result);
+
+        // Assert
+        score.Should().Be(0d);
     }
 }
