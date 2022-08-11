a356bb8cca914bc864c570f0c35734ab13b19a0d
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-08-11 07:50:40 +0200

    auth

--- src/WireMock.Net/Authentication/BasicAuthenticationMatcher.cs
+++ src/WireMock.Net/Authentication/BasicAuthenticationMatcher.cs
@@ -2,19 +2,18 @@ using System;
 using System.Text;
 using WireMock.Matchers;
 
-namespace WireMock.Authentication
+namespace WireMock.Authentication;
+
+internal class BasicAuthenticationMatcher : RegexMatcher
 {
-    internal class BasicAuthenticationMatcher : RegexMatcher
+    public BasicAuthenticationMatcher(string username, string password) : base(BuildPattern(username, password))
     {
-        public BasicAuthenticationMatcher(string username, string password) : base(BuildPattern(username, password))
-        {
-        }
+    }
 
-        public override string Name => nameof(BasicAuthenticationMatcher);
+    public override string Name => nameof(BasicAuthenticationMatcher);
 
-        private static string BuildPattern(string username, string password)
-        {
-            return "^(?i)BASIC " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password)) + "$";
-        }
+    private static string BuildPattern(string username, string password)
+    {
+        return "^(?i)BASIC " + Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password)) + "$";
     }
 }
