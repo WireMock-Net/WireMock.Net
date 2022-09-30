7ac7f0149c8ed1e38ac6647b07242ea6c7c15941
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-09-30 11:12:40 +0200

    cf2

--- test/WireMock.Net.Tests/WireMockServer.Proxy.cs
+++ test/WireMock.Net.Tests/WireMockServer.Proxy.cs
@@ -8,7 +8,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using WireMock.Admin.Mappings;
 using WireMock.Constants;
 using WireMock.Handlers;
 using WireMock.Matchers;
