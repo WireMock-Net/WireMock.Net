50bf27ea988017d75b8d1bc150e876477fa48954
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-09-30 22:41:21 +0200

    Add implicit operators to WireMockList

--- src/WireMock.Net.Abstractions/Types/WireMockList.cs
+++ src/WireMock.Net.Abstractions/Types/WireMockList.cs
@@ -1,44 +1,65 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace WireMock.Types
+namespace WireMock.Types;
+
+/// <summary>
+/// A special List which overrides the ToString() to return first value.
+/// </summary>
+/// <typeparam name="T">The generic type</typeparam>
+/// <seealso cref="List{T}" />
+public class WireMockList<T> : List<T>
 {
     /// <summary>
-    /// A special List which overrides the ToString() to return first value.
+    /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
     /// </summary>
-    /// <typeparam name="T">The generic type</typeparam>
-    /// <seealso cref="List{T}" />
-    public class WireMockList<T> : List<T>
+    public WireMockList()
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
-        /// </summary>
-        public WireMockList()
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
+    /// </summary>
+    /// <param name="collection">The collection whose elements are copied to the new list.</param>
+    public WireMockList(params T[] collection) : base(collection)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
+    /// </summary>
+    /// <param name="collection">The collection whose elements are copied to the new list.</param>
+    public WireMockList(IEnumerable<T> collection) : base(collection)
+    {
+    }
+
+    /// <summary>
+    /// Operator for setting T
+    /// </summary>
+    /// <param name="value">The value to set.</param>
+    public static implicit operator WireMockList<T>(T value) => new(value);
+
+    /// <summary>
+    /// Operator for setting T[]
+    /// </summary>
+    /// <param name="values">The values to set.</param>
+    public static implicit operator WireMockList<T>(T[] values) => new(values);
+
+    /// <summary>
+    /// Returns a <see cref="string" /> that represents this instance.
+    /// </summary>
+    public override string ToString()
+    {
+        if (Count == 1)
         {
+            if (this[0] is string strValue)
+            {
+                return strValue;
+            }
+
+            return this[0]?.ToString();
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
-        /// </summary>
-        /// <param name="collection">The collection whose elements are copied to the new list.</param>
-        public WireMockList(params T[] collection) : base(collection)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="WireMockList{T}"/> class.
-        /// </summary>
-        /// <param name="collection">The collection whose elements are copied to the new list.</param>
-        public WireMockList(IEnumerable<T> collection) : base(collection)
-        {
-        }
-
-        /// <summary>
-        /// Returns a <see cref="string" /> that represents this instance.
-        /// </summary>
-        public override string ToString()
-        {
-            return this.Any() ? this.First().ToString() : base.ToString();
-        }
+        return base.ToString();
     }
 }
