7dd3076d63507d2304c4082483aa3898d4eef215
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-01-21 15:09:31 +0100

    m

---
+++ test/WireMock.Net.Tests/Serialization/MappingConverterTests.ToCSharpCode.cs
@@ -0,0 +1,127 @@
+#if !(NET452 || NET461 || NETCOREAPP3_1)
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VerifyTests;
+using VerifyXunit;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Serialization;
+using WireMock.Types;
+using Xunit;
+
+namespace WireMock.Net.Tests.Serialization;
+
+[UsesVerify]
+public partial class MappingConverterTests
+{
+
+    [ModuleInitializer]
+    public static void ModuleInitializer()
+    {
+        VerifierSettings.DontScrubGuids();
+        VerifierSettings.DontScrubDateTimes();
+    }
+
+    [Fact]
+    public Task ToCSharpCode_With_Builder_And_AddStartIsTrue()
+    {
+        // Assign
+        var mapping = CreateMapping();
+
+        // Act
+        var code = _sut.ToCSharpCode(mapping, new MappingConverterSettings
+        {
+            AddStart = true,
+            ConverterType = MappingConverterType.Builder
+        });
+
+        // Assert
+        code.Should().NotBeEmpty();
+
+        // Verify
+        return Verifier.Verify(code);
+    }
+
+    [Fact]
+    public Task ToCSharpCode_With_Builder_And_AddStartIsFalse()
+    {
+        // Assign
+        var mapping = CreateMapping();
+
+        // Act
+        var code = _sut.ToCSharpCode(mapping, new MappingConverterSettings
+        {
+            AddStart = false,
+            ConverterType = MappingConverterType.Builder
+        });
+
+        // Assert
+        code.Should().NotBeEmpty();
+
+        // Verify
+        return Verifier.Verify(code);
+    }
+
+    [Fact]
+    public Task ToCSharpCode_With_Server_And_AddStartIsTrue()
+    {
+        // Assign
+        var mapping = CreateMapping();
+
+        // Act
+        var code = _sut.ToCSharpCode(mapping, new MappingConverterSettings
+        {
+            AddStart = true,
+            ConverterType = MappingConverterType.Server
+        });
+
+        // Assert
+        code.Should().NotBeEmpty();
+
+        // Verify
+        return Verifier.Verify(code);
+    }
+
+    [Fact]
+    public Task ToCSharpCode_With_Server_And_AddStartIsFalse()
+    {
+        // Assign
+        var mapping = CreateMapping();
+
+        // Act
+        var code = _sut.ToCSharpCode(mapping, new MappingConverterSettings
+        {
+            AddStart = false,
+            ConverterType = MappingConverterType.Server
+        });
+
+        // Assert
+        code.Should().NotBeEmpty();
+
+        // Verify
+        return Verifier.Verify(code);
+    }
+
+    private Mapping CreateMapping()
+    {
+        var guid = new Guid("8e7b9ab7-e18e-4502-8bc9-11e6679811cc");
+        var request = Request.Create()
+            .UsingGet()
+            .WithPath("test_path")
+            .WithParam("q", "42")
+            .WithClientIP("112.123.100.99")
+            .WithHeader("h-key", "h-value")
+            .WithCookie("c-key", "c-value")
+            .WithBody("b");
+        var response = Response.Create()
+            .WithHeader("Keep-Alive", "test")
+            .WithBody("bbb")
+            .WithDelay(12345)
+            .WithTransformer();
+
+        return new Mapping(guid, _updatedAt, string.Empty, string.Empty, null, _settings, request, response, 42, null, null, null, null, null, false, null);
+    }
+}
+#endif
--- test/WireMock.Net.Tests/Serialization/MappingConverterTests.cs
+++ test/WireMock.Net.Tests/Serialization/MappingConverterTests.cs
@@ -18,8 +18,7 @@ using Xunit;
 
 namespace WireMock.Net.Tests.Serialization;
 
-[UsesVerify]
-public class MappingConverterTests
+public partial class MappingConverterTests
 {
     private readonly Guid _guid = new("c8eeaf99-d5c4-4341-8543-4597c3fd40d9");
     private readonly DateTime _updatedAt = new(2022, 12, 4, 11, 12, 13);
@@ -30,13 +29,6 @@ public class MappingConverterTests
     public MappingConverterTests()
     {
         _sut = new MappingConverter(new MatcherMapper(_settings));
-    }
-
-    [ModuleInitializer]
-    public static void ModuleInitializer()
-    {
-        VerifierSettings.DontScrubGuids();
-        VerifierSettings.DontScrubDateTimes();
     }
 
     [Fact]
@@ -332,37 +324,6 @@ public class MappingConverterTests
 
         // Verify
         return Verifier.Verify(model);
-    }
-
-    [Fact]
-    public Task ToCSharpCode_UsingBuilder_Returns_Correct_Code()
-    {
-        // Assign
-        var guid = new Guid("8e7b9ab7-e18e-4502-8bc9-11e6679811cc");
-        var request = Request.Create()
-            .UsingGet()
-            .WithPath("test_path")
-            .WithParam("q", "42")
-            .WithClientIP("112.123.100.99")
-            .WithHeader("h-key", "h-value")
-            .WithCookie("c-key", "c-value")
-            .WithBody("b");
-        var response = Response.Create()
-            .WithHeader("Keep-Alive", "test")
-            .WithBody("bbb")
-            .WithDelay(12345)
-            .WithTransformer();
-
-        var mapping = new Mapping(guid, _updatedAt, string.Empty, string.Empty, null, _settings, request, response, 42, null, null, null, null, null, false, null);
-
-        // Act
-        var code = _sut.ToCSharpCode(mapping, new MappingConverterSettings { AddStart = true, ConverterType = MappingConverterType.Builder });
-
-        // Assert
-        code.Should().NotBeEmpty();
-
-        // Verify
-        return Verifier.Verify(code);
     }
 }
 #endif
