1865d3f8691d9505e30decc43886495a7faf10f0
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-06 16:00:29 +0100

    x

---
+++ src/WireMock.Net/Json/FloatBehavior.cs
@@ -0,0 +1,24 @@
+// Copied from https://github.com/Handlebars-Net/Handlebars.Net.Helpers/blob/master/src/Handlebars.Net.Helpers.DynamicLinq
+
+namespace WireMock.Json;
+
+/// <summary>
+/// Enum to define how to convert an Float in the Json Object.
+/// </summary>
+internal enum FloatBehavior
+{
+    /// <summary>
+    /// Convert all Float types in the Json Object to a double. (default)
+    /// </summary>
+    UseDouble = 0,
+
+    /// <summary>
+    /// Convert all Float types in the Json Object to a float (unless overflow).
+    /// </summary>
+    UseFloat = 1,
+
+    /// <summary>
+    /// Convert all Float types in the Json Object to a decimal (unless overflow).
+    /// </summary>
+    UseDecimal = 2
+}
