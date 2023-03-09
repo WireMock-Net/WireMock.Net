4356b9dbe95f6b22c9f9c04c36eae9a9c7934577
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-09 15:40:24 +0100

    remove

--- src/WireMock.Net/Util/JsonUtils.cs
+++ src/WireMock.Net/Util/JsonUtils.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -107,104 +105,5 @@ internal static class JsonUtils
 
             _ => throw new NotSupportedException($"Unable to convert value to {typeof(T)}.")
         };
-    }
-
-    /// <summary>
-    /// Based on / copied from Handlebars.Net Handlebars.Net.Helpers.Utils.JsonUtils
-    /// </summary>
-    public static string GenerateDynamicLinqStatement(JToken jsonObject)
-    {
-        var lines = new List<string>();
-        WalkNode(jsonObject, null, null, lines);
-
-        return lines.First();
-    }
-
-    private static void WalkNode(JToken node, string? path, string? propertyName, List<string> lines)
-    {
-        switch (node.Type)
-        {
-            case JTokenType.Object:
-                ProcessObject(node, propertyName, lines);
-                break;
-
-            case JTokenType.Array:
-                ProcessArray(node, propertyName, lines);
-                break;
-
-            default:
-                ProcessItem(node, path ?? "it", propertyName, lines);
-                break;
-        }
-    }
-
-    private static void ProcessObject(JToken node, string? propertyName, List<string> lines)
-    {
-        var items = new List<string>();
-        var text = new StringBuilder("new (");
-
-        // In case of Object, loop all children. Do a ToArray() to avoid `Collection was modified` exceptions.
-        foreach (var child in node.Children<JProperty>().ToArray())
-        {
-            WalkNode(child.Value, child.Path, child.Name, items);
-        }
-
-        text.Append(string.Join(", ", items));
-        text.Append(")");
-
-        if (!string.IsNullOrEmpty(propertyName))
-        {
-            text.AppendFormat(" as {0}", propertyName);
-        }
-
-        lines.Add(text.ToString());
-    }
-
-    private static void ProcessArray(JToken node, string? propertyName, List<string> lines)
-    {
-        var items = new List<string>();
-        var text = new StringBuilder("(new [] { ");
-
-        // In case of Array, loop all items. Do a ToArray() to avoid `Collection was modified` exceptions.
-        var idx = 0;
-        foreach (var child in node.Children().ToArray())
-        {
-            WalkNode(child, $"{node.Path}[{idx}]", null, items);
-            idx++;
-        }
-
-        text.Append(string.Join(", ", items));
-        text.Append("})");
-
-        if (!string.IsNullOrEmpty(propertyName))
-        {
-            text.AppendFormat(" as {0}", propertyName);
-        }
-
-        lines.Add(text.ToString());
-    }
-
-    private static void ProcessItem(JToken node, string path, string? propertyName, List<string> lines)
-    {
-        var castText = node.Type switch
-        {
-            JTokenType.Boolean => $"bool({path})",
-            JTokenType.Date => $"DateTime({path})",
-            JTokenType.Float => $"double({path})",
-            JTokenType.Guid => $"Guid({path})",
-            JTokenType.Integer => $"long({path})",
-            JTokenType.Null => "null",
-            JTokenType.String => $"string({path})",
-            JTokenType.TimeSpan => $"TimeSpan({path})",
-            JTokenType.Uri => $"Uri({path})",
-            _ => throw new NotSupportedException($"JTokenType '{node.Type}' cannot be converted to a Dynamic Linq cast operator.")
-        };
-
-        if (!string.IsNullOrEmpty(propertyName))
-        {
-            castText += $" as {propertyName}";
-        }
-
-        lines.Add(castText);
     }
 }
