d28e1f8932a0ff140c09d70a313fee13e112c49f
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-17 14:45:47 +0100

    add tst

--- test/WireMock.Net.Tests/WireMockServerTests.WithBody.cs
+++ test/WireMock.Net.Tests/WireMockServerTests.WithBody.cs
@@ -1,7 +1,9 @@
 #if !NET452
+//using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
+//using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using WireMock.Matchers;
@@ -10,61 +12,88 @@ using WireMock.ResponseBuilders;
 using WireMock.Server;
 using Xunit;
 
-namespace WireMock.Net.Tests
+namespace WireMock.Net.Tests;
+
+public partial class WireMockServerTests
 {
-    public partial class WireMockServerTests
+    public class DummyClass
     {
-        public class DummyClass
+        public string? Hi { get; set; }
+    }
+
+    [Fact]
+    public async Task WireMockServer_WithBodyAsJson_Using_PostAsJsonAsync_And_WildcardMatcher_ShouldMatch()
+    {
+        // Arrange
+        var server = WireMockServer.Start();
+        server.Given(
+                Request.Create().UsingPost().WithPath("/foo").WithBody(new WildcardMatcher("*Hello*"))
+            )
+            .RespondWith(
+                Response.Create().WithStatusCode(200)
+            );
+
+        var jsonObject = new DummyClass
         {
-            public string Hi { get; set; }
-        }
+            Hi = "Hello World!"
+        };
 
-        [Fact]
-        public async Task WireMockServer_WithBodyAsJson_Using_PostAsJsonAsync_And_WildcardMatcher_ShouldMatch()
-        {
-            // Arrange
-            var server = WireMockServer.Start();
-            server.Given(
-                    Request.Create().UsingPost().WithPath("/foo").WithBody(new WildcardMatcher("*Hello*"))
-                )
-                .RespondWith(
-                    Response.Create().WithStatusCode(200)
-                );
+        // Act
+        var response = await new HttpClient().PostAsJsonAsync("http://localhost:" + server.Ports[0] + "/foo", jsonObject).ConfigureAwait(false);
 
-            var jsonObject = new DummyClass
-            {
-                Hi = "Hello World!"
-            };
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-            // Act
-            var response = await new HttpClient().PostAsJsonAsync("http://localhost:" + server.Ports[0] + "/foo", jsonObject).ConfigureAwait(false);
+        server.Stop();
+    }
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
+    [Fact]
+    public async Task WireMockServer_WithBodyAsJson_Using_PostAsync_And_WildcardMatcher_ShouldMatch()
+    {
+        // Arrange
+        var server = WireMockServer.Start();
+        server.Given(
+                Request.Create().UsingPost().WithPath("/foo").WithBody(new WildcardMatcher("*Hello*"))
+            )
+            .RespondWith(
+                Response.Create().WithStatusCode(200)
+            );
 
-            server.Stop();
-        }
+        // Act
+        var response = await new HttpClient().PostAsync("http://localhost:" + server.Ports[0] + "/foo", new StringContent("{ Hi = \"Hello World\" }")).ConfigureAwait(false);
 
-        [Fact]
-        public async Task WireMockServer_WithBodyAsJson_Using_PostAsync_And_WildcardMatcher_ShouldMatch()
-        {
-            // Arrange
-            var server = WireMockServer.Start();
-            server.Given(
-                    Request.Create().UsingPost().WithPath("/foo").WithBody(new WildcardMatcher("*Hello*"))
-                )
-                .RespondWith(
-                    Response.Create().WithStatusCode(200)
-                );
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-            // Act
-            var response = await new HttpClient().PostAsync("http://localhost:" + server.Ports[0] + "/foo", new StringContent("{ Hi = \"Hello World\" }")).ConfigureAwait(false);
+        server.Stop();
+    }
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
+    [Fact]
+    public async Task WireMockServer_WithBodyAsFormUrlEncoded_Using_PostAsync_And_WithFunc()
+    {
+        // Arrange
+        var server = WireMockServer.Start();
+        server.Given(
+            Request.Create()
+                .UsingPost()
+                .WithPath("/foo")
+                .WithBody(values => values != null && values["key1"] == "value1")
+            )
+            .RespondWith(
+                Response.Create()
+            );
 
-            server.Stop();
-        }
+        // Act
+        var content = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("key1", "value1") });
+        var response = await new HttpClient()
+            .PostAsync($"{server.Url}/foo", content)
+            .ConfigureAwait(false);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        server.Stop();
     }
 }
+
 #endif
