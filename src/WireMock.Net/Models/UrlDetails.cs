810ed22764588d5def98bd745fd049311b7d091f
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-03-29 17:26:30 +0200

    urldetails

--- src/WireMock.Net/Models/UrlDetails.cs
+++ src/WireMock.Net/Models/UrlDetails.cs
@@ -1,51 +1,47 @@
-﻿using System;
+using System;
 using Stef.Validation;
 
-namespace WireMock.Models
+namespace WireMock.Models;
+
+/// <summary>
+/// UrlDetails
+/// </summary>
+public class UrlDetails
 {
     /// <summary>
-    /// UrlDetails
+    /// Gets the url (relative).
     /// </summary>
-    public class UrlDetails
+    public Uri Url { get; }
+
+    /// <summary>
+    /// Gets the AbsoluteUrl.
+    /// </summary>
+    public Uri AbsoluteUrl { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UrlDetails"/> class.
+    /// </summary>
+    /// <param name="url">The URL.</param>
+    public UrlDetails(string url) : this(new Uri(url))
     {
-        /// <summary>
-        /// Gets the url (relative).
-        /// </summary>
-        public Uri Url { get; }
+    }
 
-        /// <summary>
-        /// Gets the AbsoluteUrl.
-        /// </summary>
-        public Uri AbsoluteUrl { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UrlDetails"/> class.
+    /// </summary>
+    /// <param name="url">The URL.</param>
+    public UrlDetails(Uri url) : this(url, url)
+    {
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UrlDetails"/> class.
-        /// </summary>
-        /// <param name="url">The URL.</param>
-        public UrlDetails(string url) : this(new Uri(url))
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UrlDetails"/> class.
-        /// </summary>
-        /// <param name="url">The URL.</param>
-        public UrlDetails(Uri url) : this(url, url)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UrlDetails"/> class.
-        /// </summary>
-        /// <param name="absoluteUrl">The absolute URL.</param>
-        /// <param name="url">The URL (relative).</param>
-        public UrlDetails(Uri absoluteUrl, Uri url)
-        {
-            Guard.NotNull(absoluteUrl, nameof(absoluteUrl));
-            Guard.NotNull(url, nameof(url));
-
-            AbsoluteUrl = absoluteUrl;
-            Url = url;
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UrlDetails"/> class.
+    /// </summary>
+    /// <param name="absoluteUrl">The absolute URL.</param>
+    /// <param name="url">The URL (relative).</param>
+    public UrlDetails(Uri absoluteUrl, Uri url)
+    {
+        AbsoluteUrl = Guard.NotNull(absoluteUrl);
+        Url = Guard.NotNull(url);
     }
 }
