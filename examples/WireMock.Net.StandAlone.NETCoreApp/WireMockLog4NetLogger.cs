6b80d92ee38dca434568151633996dac5109287e
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-04-01 18:12:55 +0200

    .

--- examples/WireMock.Net.StandAlone.NETCoreApp/WireMockLog4NetLogger.cs
+++ examples/WireMock.Net.StandAlone.NETCoreApp/WireMockLog4NetLogger.cs
@@ -1,44 +1,43 @@
-﻿using System;
+using System;
 using log4net;
 using Newtonsoft.Json;
 using WireMock.Admin.Requests;
 using WireMock.Logging;
 
-namespace WireMock.Net.StandAlone.NETCoreApp
+namespace WireMock.Net.StandAlone.NETCoreApp;
+
+internal class WireMockLog4NetLogger : IWireMockLogger
 {
-    internal class WireMockLog4NetLogger : IWireMockLogger
+    private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+    public void Debug(string formatString, params object[] args)
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+        Log.DebugFormat(formatString, args);
+    }
 
-        public void Debug(string formatString, params object[] args)
-        {
-            Log.DebugFormat(formatString, args);
-        }
+    public void Info(string formatString, params object[] args)
+    {
+        Log.InfoFormat(formatString, args);
+    }
 
-        public void Info(string formatString, params object[] args)
-        {
-            Log.InfoFormat(formatString, args);
-        }
+    public void Warn(string formatString, params object[] args)
+    {
+        Log.WarnFormat(formatString, args);
+    }
 
-        public void Warn(string formatString, params object[] args)
-        {
-            Log.WarnFormat(formatString, args);
-        }
+    public void Error(string formatString, params object[] args)
+    {
+        Log.ErrorFormat(formatString, args);
+    }
 
-        public void Error(string formatString, params object[] args)
-        {
-            Log.ErrorFormat(formatString, args);
-        }
+    public void Error(string message, Exception exception)
+    {
+        Log.Error(message, exception);
+    }
 
-        public void Error(string message, Exception exception)
-        {
-            Log.Error(message, exception);
-        }
-
-        public void DebugRequestResponse(LogEntryModel logEntryModel, bool isAdminRequest)
-        {
-            string message = JsonConvert.SerializeObject(logEntryModel, Formatting.Indented);
-            Log.DebugFormat("Admin[{0}] {1}", isAdminRequest, message);
-        }
+    public void DebugRequestResponse(LogEntryModel logEntryModel, bool isAdminRequest)
+    {
+        string message = JsonConvert.SerializeObject(logEntryModel, Formatting.Indented);
+        Log.DebugFormat("Admin[{0}] {1}", isAdminRequest, message);
     }
 }
