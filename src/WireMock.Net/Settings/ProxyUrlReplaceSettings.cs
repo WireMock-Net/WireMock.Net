0a2763c06e795c2afcc323ed04ca938a22b44637
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2023-04-23 11:56:08 +0200

    Add IgnoreCase option to ProxyUrlReplaceSettings (#925)
    * Add IgnoreCase option to ProxyUrlReplaceSettings
    
    * fix

--- src/WireMock.Net/Settings/ProxyUrlReplaceSettings.cs
+++ src/WireMock.Net/Settings/ProxyUrlReplaceSettings.cs
@@ -14,4 +14,9 @@ public class ProxyUrlReplaceSettings
     /// The new path value to replace the old value with
     /// </summary>
     public string NewValue { get; set; } = null!;
+
+    /// <summary>
+    /// Defines if the case should be ignore when replacing.
+    /// </summary>
+    public bool IgnoreCase { get; set; }
 }
