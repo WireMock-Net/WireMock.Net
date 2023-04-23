0c621f9f72630768cbf34f457bd93ef3a7aa8b9b
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-04-23 10:23:54 +0200

    fix

---
+++ src/WireMock.Net/Compatibility/StringExtensions.cs
@@ -0,0 +1,15 @@
+#if NET451 || NET452 || NET46 || NET451 || NET461 || NETSTANDARD1_3 || NETSTANDARD2_0
+using System.Text.RegularExpressions;
+
+// ReSharper disable once CheckNamespace
+namespace System;
+
+internal static class StringExtensions
+{
+    public static string Replace(this string text, string oldValue, string newValue, StringComparison stringComparison)
+    {
+        var options = stringComparison == StringComparison.OrdinalIgnoreCase ? RegexOptions.IgnoreCase : RegexOptions.None;
+        return Regex.Replace(text, oldValue, newValue, options);
+    }
+}
+#endif
