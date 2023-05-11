2593431b1fed884a4af88665d1b65d0f88d9b24e
Cezary Piątek <cezarypiatek@live.com>
2023-05-11 21:38:05 +0200

    Handle object properties named as csharp keywords

--- src/WireMock.Net/Serialization/MappingConverter.cs
+++ src/WireMock.Net/Serialization/MappingConverter.cs
@@ -511,7 +511,7 @@ internal class MappingConverter
         {
             JArray jArray => FormatArray(jArray, ind),
             JObject jObject => FormatObject(jObject, ind),
-            JProperty jProperty => $"{jProperty.Name} = {ConvertJsonToAnonymousObjectDefinition(jProperty.Value, ind)}",
+            JProperty jProperty => $"{FormatPropertyName(jProperty.Name)} = {ConvertJsonToAnonymousObjectDefinition(jProperty.Value, ind)}",
             JValue jValue => jValue.Type switch
             {
                 JTokenType.None => "null",
@@ -526,6 +526,11 @@ internal class MappingConverter
             },
             _ => $"UNHANDLED_CASE: {token}"
         };
+    }
+
+    private static string FormatPropertyName(string propertyName)
+    {
+        return csharp_keywords.Contains(propertyName) ? "@" + propertyName : propertyName;
     }
 
     private static string FormatObject(JObject jObject, int ind)
@@ -550,4 +555,85 @@ internal class MappingConverter
 
         return $"new [] {{ {string.Join(", ", items)} }}";
     }
+
+    private static readonly HashSet<string> csharp_keywords = new HashSet<string>(new[]
+    {
+        "abstract",
+        "as",
+        "base",
+        "bool",
+        "break",
+        "byte",
+        "case",
+        "catch",
+        "char",
+        "checked",
+        "class",
+        "const",
+        "continue",
+        "decimal",
+        "default",
+        "delegate",
+        "do",
+        "double",
+        "else",
+        "enum",
+        "event",
+        "explicit",
+        "extern",
+        "false",
+        "finally",
+        "fixed",
+        "float",
+        "for",
+        "foreach",
+        "goto",
+        "if",
+        "implicit",
+        "in",
+        "int",
+        "interface",
+        "internal",
+        "is",
+        "lock",
+        "long",
+        "namespace",
+        "new",
+        "null",
+        "object",
+        "operator",
+        "out",
+        "override",
+        "params",
+        "private",
+        "protected",
+        "public",
+        "readonly",
+        "ref",
+        "return",
+        "sbyte",
+        "sealed",
+        "short",
+        "sizeof",
+        "stackalloc",
+        "static",
+        "string",
+        "struct",
+        "switch",
+        "this",
+        "throw",
+        "true",
+        "try",
+        "typeof",
+        "uint",
+        "ulong",
+        "unchecked",
+        "unsafe",
+        "ushort",
+        "using",
+        "virtual",
+        "void",
+        "volatile",
+        "while"
+    });
 }
