3da300042d36639b99a7319d41cce51a8e3e5b8e
Cezary Piątek <cezarypiatek@live.com>
2023-05-12 19:48:59 +0200

    Refactor: Extract logic responsible for generating anonymous object definition to a separate class

--- src/WireMock.Net/Serialization/MappingConverter.cs
+++ src/WireMock.Net/Serialization/MappingConverter.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Stef.Validation;
 using WireMock.Admin.Mappings;
 using WireMock.Constants;
@@ -20,6 +18,7 @@ using WireMock.Settings;
 using WireMock.Types;
 using WireMock.Util;
 
+using static WireMock.Util.CSharpFormatter;
 namespace WireMock.Serialization;
 
 internal class MappingConverter
@@ -160,12 +159,8 @@ internal class MappingConverter
                     }
                     else if(bodyData.BodyAsJson is {} jsonBody)
                     {
-                        var serializedBody = JsonConvert.SerializeObject(jsonBody);
-                        using var jsonReader = new JsonTextReader(new StringReader(serializedBody));
-                        jsonReader.DateParseHandling = DateParseHandling.None;
-                        var deserializedBody = JObject.Load(jsonReader);
-
-                        sb.AppendLine($"        .WithBodyAsJson({ConvertJsonToAnonymousObjectDefinition(deserializedBody, 2)})");
+                        var anonymousObjectDefinition = ConvertToAnonymousObjectDefinition(jsonBody);
+                        sb.AppendLine($"        .WithBodyAsJson({anonymousObjectDefinition})");
                     }
 
                     break;
@@ -466,16 +461,6 @@ internal class MappingConverter
         return values is { } ? string.Join(", ", values.Select(ToCSharpStringLiteral)) : ToCSharpStringLiteral(defaultValue);
     }
 
-    private static string ToCSharpStringLiteral(string? value)
-    {
-        var escapedValue = value?.Replace("\"", "\\\"") ?? string.Empty;
-        if (escapedValue.Contains("\n"))
-        {
-            return $"@\"{escapedValue}\"";
-        }
-        return $"\"{escapedValue}\"";
-    }
-
     private static WebProxyModel? MapWebProxy(WebProxySettings? settings)
     {
         return settings != null ? new WebProxyModel
@@ -505,135 +490,5 @@ internal class MappingConverter
         return newDictionary;
     }
 
-    private static string ConvertJsonToAnonymousObjectDefinition(JToken token, int ind = 0)
-    {
-        return token switch
-        {
-            JArray jArray => FormatArray(jArray, ind),
-            JObject jObject => FormatObject(jObject, ind),
-            JProperty jProperty => $"{FormatPropertyName(jProperty.Name)} = {ConvertJsonToAnonymousObjectDefinition(jProperty.Value, ind)}",
-            JValue jValue => jValue.Type switch
-            {
-                JTokenType.None => "null",
-                JTokenType.Integer => jValue.Value?.ToString() ?? "null",
-                JTokenType.Float => jValue.Value?.ToString() ?? "null",
-                JTokenType.String => ToCSharpStringLiteral(jValue.Value?.ToString()),
-                JTokenType.Boolean => jValue.Value?.ToString()?.ToLower() ?? "null",
-                JTokenType.Null => "null",
-                JTokenType.Undefined => "null",
-                JTokenType.Date when jValue.Value is DateTime dateValue => $"DateTime.Parse({ToCSharpStringLiteral(dateValue.ToString("s"))})",
-                _ => $"UNHANDLED_CASE: {jValue.Type}"
-            },
-            _ => $"UNHANDLED_CASE: {token}"
-        };
-    }
 
-    private static string FormatPropertyName(string propertyName)
-    {
-        return csharp_keywords.Contains(propertyName) ? "@" + propertyName : propertyName;
-    }
-
-    private static string FormatObject(JObject jObject, int ind)
-    {
-        var indStr = new string(' ', 4 * ind);
-        var indStrSub = new string(' ', 4 * (ind + 1));
-        var items = jObject.Properties().Select(x => ConvertJsonToAnonymousObjectDefinition(x, ind + 1));
-
-        return $"new\r\n{indStr}{{\r\n{indStrSub}{string.Join($",\r\n{indStrSub}", items)}\r\n{indStr}}}";
-    }
-
-    private static string FormatArray(JArray jArray, int ind)
-    {
-        var hasComplexItems = jArray.FirstOrDefault() is JObject or JArray;
-        var items = jArray.Select(x => ConvertJsonToAnonymousObjectDefinition(x, hasComplexItems ? ind + 1 : ind));
-        if (hasComplexItems)
-        {
-            var indStr = new string(' ', 4 * ind);
-            var indStrSub = new string(' ', 4 * (ind + 1));
-            return $"new []\r\n{indStr}{{\r\n{indStrSub}{string.Join($",\r\n{indStrSub}", items)}\r\n{indStr}}}";
-        }
-
-        return $"new [] {{ {string.Join(", ", items)} }}";
-    }
-
-    private static readonly HashSet<string> csharp_keywords = new HashSet<string>(new[]
-    {
-        "abstract",
-        "as",
-        "base",
-        "bool",
-        "break",
-        "byte",
-        "case",
-        "catch",
-        "char",
-        "checked",
-        "class",
-        "const",
-        "continue",
-        "decimal",
-        "default",
-        "delegate",
-        "do",
-        "double",
-        "else",
-        "enum",
-        "event",
-        "explicit",
-        "extern",
-        "false",
-        "finally",
-        "fixed",
-        "float",
-        "for",
-        "foreach",
-        "goto",
-        "if",
-        "implicit",
-        "in",
-        "int",
-        "interface",
-        "internal",
-        "is",
-        "lock",
-        "long",
-        "namespace",
-        "new",
-        "null",
-        "object",
-        "operator",
-        "out",
-        "override",
-        "params",
-        "private",
-        "protected",
-        "public",
-        "readonly",
-        "ref",
-        "return",
-        "sbyte",
-        "sealed",
-        "short",
-        "sizeof",
-        "stackalloc",
-        "static",
-        "string",
-        "struct",
-        "switch",
-        "this",
-        "throw",
-        "true",
-        "try",
-        "typeof",
-        "uint",
-        "ulong",
-        "unchecked",
-        "unsafe",
-        "ushort",
-        "using",
-        "virtual",
-        "void",
-        "volatile",
-        "while"
-    });
 }
