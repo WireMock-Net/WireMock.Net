efb18397d099de9a8cde2a641e6e3bba32d666be
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-08-12 10:08:44 +0200

    Add extra unit test for WithParam multiple values comma

--- test/WireMock.Net.Tests/WireMockServerTests.WithParam.cs
+++ test/WireMock.Net.Tests/WireMockServerTests.WithParam.cs
@@ -16,6 +16,7 @@ public partial class WireMockServerTests
     [Theory]
     [InlineData("SELECT id, value FROM table WHERE id = 1")]
     [InlineData("select id, name, value from table where id in (1, 2, 3, 4, 5)")]
+    [InlineData("1,2,3")]
     public async Task WireMockServer_WithParam_QueryParameterMultipleValueSupport_NoComma_Should_Ignore_Comma(string queryValue)
     {
         // Arrange
@@ -29,6 +30,32 @@ public partial class WireMockServerTests
                     .UsingGet()
                     .WithPath("/foo")
                     .WithParam("query", queryValue)
+            )
+            .RespondWith(
+                Response.Create().WithStatusCode(200)
+            );
+
+        // Act
+        var requestUri = new Uri($"http://localhost:{server.Port}/foo?query={queryValue}");
+        var response = await server.CreateClient().GetAsync(requestUri).ConfigureAwait(false);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        server.Stop();
+    }
+
+    [Fact]
+    public async Task WireMockServer_WithParam_MultiValueComma()
+    {
+        // Arrange
+        var queryValue = "1,2,3";
+        var server = WireMockServer.Start();
+        server.Given(
+                Request.Create()
+                    .UsingGet()
+                    .WithPath("/foo")
+                    .WithParam("query", "1", "2", "3")
             )
             .RespondWith(
                 Response.Create().WithStatusCode(200)
