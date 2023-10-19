4496e124ff6d5ccc141c2394d0b77ab36820e245
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-10-19 08:05:43 +0200

    add or set

--- src/WireMock.Net/Util/ReflectionUtils.cs
+++ src/WireMock.Net/Util/ReflectionUtils.cs
@@ -18,44 +18,31 @@ internal static class ReflectionUtils
         TypeAttributes.BeforeFieldInit |
         TypeAttributes.AutoLayout;
     private static readonly ConcurrentDictionary<string, Type> TypeCache = new();
-    /*
-     , 
-     */
 
     public static Type CreateType(string typeName, Type? parentType = null)
     {
-        // Check if the type has already been created
-        if (TypeCache.TryGetValue(typeName, out var existingType))
+        return TypeCache.GetOrAdd(typeName, key =>
         {
-            return existingType;
-        }
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(AssemblyName, AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(DynamicModuleName);
 
-        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(AssemblyName, AssemblyBuilderAccess.Run);
-        var moduleBuilder = assemblyBuilder.DefineDynamicModule(DynamicModuleName);
+            var typeBuilder = moduleBuilder.DefineType(key, ClassAttributes, parentType);
 
-        var typeBuilder = moduleBuilder.DefineType(typeName, ClassAttributes, parentType);
-
-        // Create the type and cache it
-        var newType = typeBuilder.CreateTypeInfo().AsType();
-        TypeCache[typeName] = newType;
-
-        return newType;
+            // Create the type and cache it
+            return typeBuilder.CreateTypeInfo()!.AsType();
+        });
     }
 
     public static Type CreateGenericType(string typeName, Type genericTypeDefinition, params Type[] typeArguments)
     {
-        var key = $"{typeName}_{genericTypeDefinition.Name}_{string.Join(", ", typeArguments.Select(t => t.Name))}";
-        if (TypeCache.TryGetValue(key, out var existingType))
+        var genericKey = $"{typeName}_{genericTypeDefinition.Name}_{string.Join(", ", typeArguments.Select(t => t.Name))}";
+
+        return TypeCache.GetOrAdd(genericKey, _ =>
         {
-            return existingType;
-        }
+            var genericType = genericTypeDefinition.MakeGenericType(typeArguments);
 
-        var genericType = genericTypeDefinition.MakeGenericType(typeArguments);
-
-        // Create the type based on the genericType and cache it
-        var newType = CreateType(typeName, genericType);
-        TypeCache[key] = newType;
-
-        return newType;
+            // Create the type based on the genericType and cache it
+            return CreateType(typeName, genericType);
+        });
     }
 }
