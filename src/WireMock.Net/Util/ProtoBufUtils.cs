980595c9c51e862673900eb791c894013ba1010f
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-12-28 11:16:12 +0100

    6

--- src/WireMock.Net/Util/ProtoBufUtils.cs
+++ src/WireMock.Net/Util/ProtoBufUtils.cs
@@ -1,14 +1,12 @@
 #if PROTOBUF
-using System;
-using System.Buffers.Binary;
 using JsonConverter.Abstractions;
+using ProtoBufJsonConverter;
+using ProtoBufJsonConverter.Models;
 
 namespace WireMock.Util;
 
 internal static class ProtoBufUtils
 {
-    private const int HeaderSize = 5; // 1 (Compression flag) + 4 (UInt32)
-
     internal static byte[] GetProtoBufMessageWithHeader(
         string protoDefinition,
         string messageType,
@@ -17,7 +15,8 @@ internal static class ProtoBufUtils
         JsonConverterOptions? options
     )
     {
-        var request = new ProtoBufJsonConverter.Models.ConvertToProtoBufRequest(protoDefinition, messageType, value);
+        var request = new ConvertToProtoBufRequest(protoDefinition, messageType, value, true);
+
         if (jsonConverter != null)
         {
             request = request.WithJsonConverter(jsonConverter);
@@ -27,24 +26,7 @@ internal static class ProtoBufUtils
             }
         }
 
-        var protobufMessageBytes = SingletonFactory<ProtoBufJsonConverter.Converter>.GetInstance().Convert(request);
-
-        var protobufMessageWithHeaderBytes = new byte[HeaderSize + protobufMessageBytes.Length];
-
-        WriteHeader(protobufMessageWithHeaderBytes, protobufMessageBytes.Length);
-
-        Buffer.BlockCopy(protobufMessageBytes, 0, protobufMessageWithHeaderBytes, HeaderSize, protobufMessageBytes.Length);
-
-        return protobufMessageWithHeaderBytes;
-    }
-
-    private static void WriteHeader(Span<byte> headerData, int length)
-    {
-        // Compression flag
-        headerData[0] = 0;
-
-        // Message length
-        BinaryPrimitives.WriteUInt32BigEndian(headerData.Slice(1), (uint)length);
+        return SingletonFactory<Converter>.GetInstance().Convert(request);
     }
 }
 #endif
