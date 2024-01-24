e8b2218924b63f0b6f7eaee0525e76d593e5f3b5
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-01-24 08:05:31 +0100

    Fix some SonarCloud issues

--- src/WireMock.Net.OpenApiParser/Settings/WireMockOpenApiParserDynamicExampleValues.cs
+++ src/WireMock.Net.OpenApiParser/Settings/WireMockOpenApiParserDynamicExampleValues.cs
@@ -11,31 +11,31 @@ namespace WireMock.Net.OpenApiParser.Settings;
 public class WireMockOpenApiParserDynamicExampleValues : IWireMockOpenApiParserExampleValues
 {
     /// <inheritdoc />
-    public virtual bool Boolean { get => RandomizerFactory.GetRandomizer(new FieldOptionsBoolean()).Generate() ?? true; set { } }
+    public virtual bool Boolean => RandomizerFactory.GetRandomizer(new FieldOptionsBoolean()).Generate() ?? true;
 
     /// <inheritdoc />
-    public virtual int Integer { get => RandomizerFactory.GetRandomizer(new FieldOptionsInteger()).Generate() ?? 42; set { } }
+    public virtual int Integer => RandomizerFactory.GetRandomizer(new FieldOptionsInteger()).Generate() ?? 42;
 
     /// <inheritdoc />
-    public virtual float Float { get => RandomizerFactory.GetRandomizer(new FieldOptionsFloat()).Generate() ?? 4.2f; set { } }
+    public virtual float Float => RandomizerFactory.GetRandomizer(new FieldOptionsFloat()).Generate() ?? 4.2f;
 
     /// <inheritdoc />
-    public virtual double Double { get => RandomizerFactory.GetRandomizer(new FieldOptionsDouble()).Generate() ?? 4.2d; set { } }
+    public virtual double Double => RandomizerFactory.GetRandomizer(new FieldOptionsDouble()).Generate() ?? 4.2d;
 
     /// <inheritdoc />
-    public virtual Func<DateTime> Date { get { return () => RandomizerFactory.GetRandomizer(new FieldOptionsDateTime()).Generate() ?? System.DateTime.UtcNow.Date; } set { } }
+    public virtual Func<DateTime> Date { get { return () => RandomizerFactory.GetRandomizer(new FieldOptionsDateTime()).Generate() ?? System.DateTime.UtcNow.Date; } }
 
     /// <inheritdoc />
-    public virtual Func<DateTime> DateTime { get { return () => RandomizerFactory.GetRandomizer(new FieldOptionsDateTime()).Generate() ?? System.DateTime.UtcNow; } set { } }
+    public virtual Func<DateTime> DateTime { get { return () => RandomizerFactory.GetRandomizer(new FieldOptionsDateTime()).Generate() ?? System.DateTime.UtcNow; } }
 
     /// <inheritdoc />
-    public virtual byte[] Bytes { get => RandomizerFactory.GetRandomizer(new FieldOptionsBytes()).Generate(); set { } }
+    public virtual byte[] Bytes => RandomizerFactory.GetRandomizer(new FieldOptionsBytes()).Generate();
 
     /// <inheritdoc />
-    public virtual object Object { get; set; } = "example-object";
+    public virtual object Object => "example-object";
 
     /// <inheritdoc />
-    public virtual string String { get => RandomizerFactory.GetRandomizer(new FieldOptionsTextRegex { Pattern = @"^[0-9]{2}[A-Z]{5}[0-9]{2}" }).Generate() ?? "example-string"; set { } }
+    public virtual string String => RandomizerFactory.GetRandomizer(new FieldOptionsTextRegex { Pattern = @"^[0-9]{2}[A-Z]{5}[0-9]{2}" }).Generate() ?? "example-string";
 
     /// <inheritdoc />
     public virtual OpenApiSchema? Schema { get; set; }
--- src/WireMock.Net/RequestBuilders/Request.ClientIP.cs
+++ src/WireMock.Net/RequestBuilders/Request.ClientIP.cs
@@ -23,17 +23,17 @@ public partial class Request
     }
 
     /// <inheritdoc />
-    public IRequestBuilder WithClientIP(params string[] paths)
+    public IRequestBuilder WithClientIP(params string[] clientIPs)
     {
-        return WithClientIP(MatchOperator.Or, paths);
+        return WithClientIP(MatchOperator.Or, clientIPs);
     }
 
     /// <inheritdoc />
-    public IRequestBuilder WithClientIP(MatchOperator matchOperator, params string[] paths)
+    public IRequestBuilder WithClientIP(MatchOperator matchOperator, params string[] clientIPs)
     {
-        Guard.NotNullOrEmpty(paths);
+        Guard.NotNullOrEmpty(clientIPs);
 
-        _requestMatchers.Add(new RequestMessageClientIPMatcher(MatchBehaviour.AcceptOnMatch, matchOperator, paths));
+        _requestMatchers.Add(new RequestMessageClientIPMatcher(MatchBehaviour.AcceptOnMatch, matchOperator, clientIPs));
         return this;
     }
 
