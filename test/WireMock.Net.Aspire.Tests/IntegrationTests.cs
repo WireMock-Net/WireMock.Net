62cc3a25137867c5351728a241ba6d42260b3cc5
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-05-25 12:41:31 +0200

    Admin

---
+++ test/WireMock.Net.Aspire.Tests/IntegrationTests.cs
@@ -0,0 +1,57 @@
+using System.Net.Http.Json;
+using FluentAssertions;
+using Projects;
+using Xunit.Abstractions;
+
+namespace WireMock.Net.Aspire.Tests;
+
+public class IntegrationTests(ITestOutputHelper output)
+{
+    private record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
+
+    [Fact]
+    public async Task StartAppHostWithWireMockAndCreateHttpClientToCallTheMockedWeatherForecastEndpoint()
+    {
+        if (!DockerUtils.IsDockerRunningLinuxContainerMode())
+        {
+            output.WriteLine("Docker is not running in Linux container mode. Skipping test.");
+            return;
+        }
+
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<WireMock_Net_Aspire_TestAppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        using var httpClient = app.CreateHttpClient("wiremock-service");
+        var weatherForecasts = await httpClient.GetFromJsonAsync<WeatherForecast[]>("/weatherforecast");
+
+        // Assert
+        weatherForecasts.Should().BeEquivalentTo(new[]
+        {
+            new WeatherForecast(new DateOnly(2024, 5, 24), -10, "Freezing"),
+            new WeatherForecast(new DateOnly(2024, 5, 25), 33, "Hot")
+        });
+    }
+
+    [Fact]
+    public async Task StartAppHostWithWireMockAndCreateWireMockAdminClientToCallTheAdminEndpoint()
+    {
+        if (!DockerUtils.IsDockerRunningLinuxContainerMode())
+        {
+            output.WriteLine("Docker is not running in Linux container mode. Skipping test.");
+            return;
+        }
+
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<WireMock_Net_Aspire_TestAppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        var adminClient = app.CreateWireMockAdminClient("wiremock-service");
+        var settings = await adminClient.GetSettingsAsync();
+        settings.Should().NotBeNull();
+    }
+}
--- test/WireMock.Net.Aspire.Tests/WeatherForecast.cs
+++
@@ -1,3 +0,0 @@
-namespace WireMock.Net.Aspire.Tests;
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
