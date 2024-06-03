8eda46ffc788290083e750c5ba57c451a9f6b5fc
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-06-03 11:01:46 +0200

    1.5.56

--- Generate-ReleaseNotes.cmd
+++ Generate-ReleaseNotes.cmd
@@ -1,6 +1,6 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=1.5.55
+SET version=1.5.56
 
 GitHubReleaseNotes --output CHANGELOG.md --skip-empty-releases --exclude-labels question invalid doc duplicate example --version %version% --token %GH_TOKEN%
 
