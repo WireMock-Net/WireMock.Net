45ad87cc89d8609d26ace79bd4b0ad8e219aefc5
cocoon <cocoon@users.noreply.github.com>
2024-09-06 09:49:07 +0200

    add using System.Net.Sockets

--- test/WireMock.Net.Tests/WireMockServerTests.cs
+++ test/WireMock.Net.Tests/WireMockServerTests.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
