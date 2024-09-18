31516a75c4102a80718874504792e4f870fe1317
Ruxo Zheng <rz@just.sent.as>
2024-09-19 05:56:34 +0700

    Remove excessive null forgiving operators

--- src/WireMock.Net.Abstractions/Models/IBodyDataExtension.cs
+++ src/WireMock.Net.Abstractions/Models/IBodyDataExtension.cs
@@ -7,10 +7,10 @@ public static class IBodyDataExtension
 {
     public static BodyType GetBodyType(this IBodyData bodyData) {
         if (bodyData.DetectedBodyTypeFromContentType is not null and not BodyType.None) {
-            return bodyData.DetectedBodyTypeFromContentType!.Value;
+            return bodyData.DetectedBodyTypeFromContentType.Value;
         }
         if (bodyData.DetectedBodyType is not null and not BodyType.None) {
-            return bodyData.DetectedBodyType!.Value;
+            return bodyData.DetectedBodyType.Value;
         }
         return BodyType.None;
     }
