8e0e2d27aff8a30fa0d79a36926a8b4a67593c75
Ruxo Zheng <rz@just.sent.as>
2024-09-19 16:07:05 +0700

    Adjust braces

--- src/WireMock.Net.Abstractions/Models/IBodyDataExtension.cs
+++ src/WireMock.Net.Abstractions/Models/IBodyDataExtension.cs
@@ -5,11 +5,14 @@ namespace WireMock.Util;
 
 public static class IBodyDataExtension
 {
-    public static BodyType GetBodyType(this IBodyData bodyData) {
-        if (bodyData.DetectedBodyTypeFromContentType is not null and not BodyType.None) {
+    public static BodyType GetBodyType(this IBodyData bodyData)
+    {
+        if (bodyData.DetectedBodyTypeFromContentType is not null and not BodyType.None)
+        {
             return bodyData.DetectedBodyTypeFromContentType.Value;
         }
-        if (bodyData.DetectedBodyType is not null and not BodyType.None) {
+        if (bodyData.DetectedBodyType is not null and not BodyType.None)
+        {
             return bodyData.DetectedBodyType.Value;
         }
         return BodyType.None;
