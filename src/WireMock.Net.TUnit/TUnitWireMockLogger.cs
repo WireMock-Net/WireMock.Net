0a2ac830c2551e07814d0fc6914607047d2073df
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-09-22 11:56:01 +0200

    Add WireMock.Net.TUnit project

---
+++ src/WireMock.Net.TUnit/TUnitWireMockLogger.cs
@@ -0,0 +1,82 @@
+// Copyright © WireMock.Net
+
+using System;
+using Newtonsoft.Json;
+using Stef.Validation;
+using TUnit.Core.Logging;
+using WireMock.Admin.Requests;
+using WireMock.Logging;
+
+namespace WireMock.Net.TUnit;
+
+/// <summary>
+/// When using TUnit, this class enables to log the output from WireMock.Net to the <see cref="TUnitLogger"/>.
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public sealed class TUnitWireMockLogger : IWireMockLogger
+{
+    private readonly TUnitLogger _tUnitLogger;
+
+    /// <summary>
+    /// Create a new instance on the <see cref="TUnitWireMockLogger"/>.
+    /// </summary>
+    /// <param name="tUnitLogger">Represents a class which can be used to provide test output.</param>
+    public TUnitWireMockLogger(TUnitLogger tUnitLogger)
+    {
+        _tUnitLogger = Guard.NotNull(tUnitLogger);
+    }
+
+    /// <inheritdoc />
+    public void Debug(string formatString, params object[] args)
+    {
+        _tUnitLogger.LogDebug(Format("Debug", formatString, args));
+    }
+
+    /// <inheritdoc />
+    public void Info(string formatString, params object[] args)
+    {
+        _tUnitLogger.LogInformation(Format("Info", formatString, args));
+    }
+
+    /// <inheritdoc />
+    public void Warn(string formatString, params object[] args)
+    {
+        _tUnitLogger.LogWarning(Format("Warning", formatString, args));
+    }
+
+    /// <inheritdoc />
+    public void Error(string formatString, params object[] args)
+    {
+        _tUnitLogger.LogError(Format("Error", formatString, args));
+    }
+
+    /// <inheritdoc />
+    public void Error(string formatString, Exception exception)
+    {
+        _tUnitLogger.LogError(Format("Error", formatString, exception.Message), exception);
+
+        if (exception is AggregateException ae)
+        {
+            ae.Handle(ex =>
+            {
+                _tUnitLogger.LogError(Format("Error", "Exception {0}", ex.Message), exception);
+                return true;
+            });
+        }
+    }
+
+    /// <inheritdoc />
+    public void DebugRequestResponse(LogEntryModel logEntryModel, bool isAdminRequest)
+    {
+        var message = JsonConvert.SerializeObject(logEntryModel, Formatting.Indented);
+        _tUnitLogger.LogDebug(Format("DebugRequestResponse", "Admin[{0}] {1}", isAdminRequest, message));
+    }
+
+    private static string Format(string level, string formatString, params object[] args)
+    {
+        Guard.NotNull(formatString);
+
+        var message = args.Length > 0 ? string.Format(formatString, args) : formatString;
+        return $"{DateTime.UtcNow} [{level}] : {message}";
+    }
+}
---
+++ test/WireMock.Net.Aspire.TUnitTests/TUnitTests.cs
@@ -0,0 +1,46 @@
+// Copyright © WireMock.Net
+
+using WireMock.Logging;
+using WireMock.Net.TUnit;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace WireMock.Net.Aspire.TUnitTests;
+
+// ReSharper disable once InconsistentNaming
+public class TUnitTests
+{
+    private IWireMockLogger _logger = null!;
+
+    [Before(Test)]
+    public void BeforeTest(TestContext context)
+    {
+        _logger = new TUnitWireMockLogger(context.GetDefaultLogger());
+    }
+
+    [Test]
+    public async Task Test_TUnitWireMockLogger()
+    {
+        // Assign
+        var path = $"/foo_{Guid.NewGuid()}";
+
+        using var server = WireMockServer.Start(new WireMockServerSettings
+        {
+            Logger = _logger
+        });
+
+        server
+            .Given(Request.Create()
+                .WithPath(path)
+                .UsingGet())
+            .RespondWith(Response.Create().WithBody("TUnit"));
+
+        // Act
+        var response = await new HttpClient().GetStringAsync($"{server.Url}{path}").ConfigureAwait(false);
+
+        // Assert
+        await Assert.That(response).IsEqualTo("TUnit");
+    }
+}
