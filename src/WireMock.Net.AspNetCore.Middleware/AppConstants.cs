42306d1864396643a9a5dc021d25508a31d66cd9
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2024-09-27 20:39:57 +0200

    Add WireMock.Net.AspNetCore.Middleware (#1175)
    * Add WireMock.Net.AspNetCore.Middleware
    
    * .
    
    * WireMock.Net.Middleware.Tests
    
    * .
    
    * X-WireMock-Response-Delay

---
+++ src/WireMock.Net.AspNetCore.Middleware/AppConstants.cs
@@ -0,0 +1,9 @@
+// Copyright © WireMock.Net
+
+namespace WireMock.Net.AspNetCore.Middleware;
+
+internal static class AppConstants
+{
+    internal const string HEADER_REDIRECT = "X-WireMock-Redirect";
+    internal const string HEADER_RESPONSE_DELAY = "X-WireMock-Response-Delay";
+}
---
+++ src/WireMock.Net.AspNetCore.Middleware/HttpDelegatingHandler/WireMockDelegationHandlerSettings.cs
@@ -0,0 +1,8 @@
+// Copyright © WireMock.Net
+
+namespace WireMock.Net.AspNetCore.Middleware.HttpDelegatingHandler;
+
+internal class WireMockDelegationHandlerSettings
+{
+    public bool AlwaysRedirect { get; set; }
+}
