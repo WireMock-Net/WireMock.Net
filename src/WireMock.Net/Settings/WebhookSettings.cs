bb456fbf9a35eda9010144e8951091f610ed3fea
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-12-22 17:28:50 +0100

    Add functionality to call a PostTransform method after the Webhook request has been transformed

--- src/WireMock.Net/Settings/WebhookSettings.cs
+++ src/WireMock.Net/Settings/WebhookSettings.cs
@@ -1,5 +1,9 @@
 // Copyright © WireMock.Net
 
+using System.Collections.Generic;
+using WireMock.Types;
+using WireMock.Util;
+
 namespace WireMock.Settings;
 
 /// <summary>
@@ -7,4 +11,14 @@ namespace WireMock.Settings;
 /// </summary>
 public class WebhookSettings : HttpClientSettings
 {
+    /// <summary>
+    /// Executes an action after the transformation of the request body.
+    /// </summary>
+    /// <param name="mapping">The mapping used for the request.</param>
+    /// <param name="requestUrl">The request Url.</param>
+    /// <param name="bodyData">The body data of the request. [Optional]</param>
+    /// <param name="headers">The headers of the request. [Optional]</param>
+    public virtual void PostTransform(IMapping mapping, string requestUrl, IBodyData? bodyData = null, IDictionary<string, WireMockList<string>>? headers = null)
+    {
+    }
 }
