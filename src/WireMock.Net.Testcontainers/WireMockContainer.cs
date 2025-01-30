fb28796f1c59abcc7d15da5869834d21eace30bb
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2025-01-30 15:07:39 +0100

    "

--- src/WireMock.Net.Testcontainers/WireMockContainer.cs
+++ src/WireMock.Net.Testcontainers/WireMockContainer.cs
@@ -221,7 +221,7 @@ public sealed class WireMockContainer : DockerContainer
     {
         foreach (var kvp in _configuration.ProtoDefinitions)
         {
-            Logger.LogInformation("Adding ProtoDefinition {Id}', kvp.Key);
+            Logger.LogInformation("Adding ProtoDefinition {Id}", kvp.Key);
             foreach (var protoDefinition in kvp.Value)
             {
                 await _adminApi!.AddProtoDefinitionAsync(kvp.Key, protoDefinition);
