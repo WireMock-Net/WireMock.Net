0effda3cfa3f8c134d5c0810d912509f039bbdf7
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-04 18:11:13 +0100

    1.8.0-prview-01 (Update the usage from the custom Handlebars.Net File helper)

--- Generate-ReleaseNotes.cmd
+++ Generate-ReleaseNotes.cmd
@@ -1,6 +1,6 @@
 rem https://github.com/StefH/GitHubReleaseNotes
 
-SET version=1.7.4
+SET version=1.8.0-prview-01
 
 GitHubReleaseNotes --output CHANGELOG.md --skip-empty-releases --exclude-labels test question invalid doc duplicate example environment --version %version% --token %GH_TOKEN%
 
