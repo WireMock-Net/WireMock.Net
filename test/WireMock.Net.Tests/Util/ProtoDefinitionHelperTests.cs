676a1ea17b156d53d20c725023320c2d3b778ad0
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-13 19:31:18 +0100

    unix-windows

--- test/WireMock.Net.Tests/Util/ProtoDefinitionHelperTests.cs
+++ test/WireMock.Net.Tests/Util/ProtoDefinitionHelperTests.cs
@@ -12,6 +12,8 @@ public class ProtoDefinitionHelperTests
     {
         // Arrange
         var directory = Path.Combine(Directory.GetCurrentDirectory(), "Grpc", "Test");
+        var expectedFilename = $"SubFolder{Path.DirectorySeparatorChar}request.proto";
+        var expectedComment = $"// {expectedFilename}";
 
         // Act
         var protoDefinitions = ProtoDefinitionHelper.FromDirectory(directory);
@@ -19,19 +21,19 @@ public class ProtoDefinitionHelperTests
         // Assert
         protoDefinitions.Should().HaveCount(2);
         protoDefinitions[0].Should().StartWith("// greet.proto");
-        protoDefinitions[1].Should().StartWith("// SubFolder\\request.proto");
+        protoDefinitions[1].Should().StartWith(expectedComment);
 
 #if PROTOBUF
         // Arrange
         var resolver = new WireMockProtoFileResolver(protoDefinitions);
 
         // Act + Assert
-        resolver.Exists("SubFolder\\request.proto").Should().BeTrue();
+        resolver.Exists(expectedFilename).Should().BeTrue();
         resolver.Exists("x").Should().BeFalse();
 
         // Act + Assert
-        var text = resolver.OpenText("SubFolder\\request.proto").ReadToEnd();
-        text.Should().StartWith("// SubFolder\\request.proto");
+        var text = resolver.OpenText(expectedFilename).ReadToEnd();
+        text.Should().StartWith(expectedComment);
         System.Action action = () => resolver.OpenText("x");
         action.Should().Throw<FileNotFoundException>();
 #endif
