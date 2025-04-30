779b23c1bd8e54deb175c3c60ba4a428ae4490ba
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-04-30 18:29:28 +0200

    Update AzureADAuthenticationMatcher to support V2 Azure AAD tokens

--- src/WireMock.Net/Authentication/AzureADAuthenticationMatcher.cs
+++ src/WireMock.Net/Authentication/AzureADAuthenticationMatcher.cs
@@ -2,6 +2,7 @@
 
 #if !NETSTANDARD1_3
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text.RegularExpressions;
@@ -19,18 +20,25 @@ namespace WireMock.Authentication;
 /// <summary>
 /// https://www.c-sharpcorner.com/article/how-to-validate-azure-ad-token-using-console-application/
 /// https://stackoverflow.com/questions/38684865/validation-of-an-azure-ad-bearer-token-in-a-console-application
+/// https://github.com/AzureAD/microsoft-identity-web/blob/36fb5f555638787823a89e89c67f17d6a10006ed/tools/CrossPlatformValidator/CrossPlatformValidation/CrossPlatformValidation/RequestValidator.cs#L42
 /// </summary>
 internal class AzureADAuthenticationMatcher : IStringMatcher
 {
     private const string BearerPrefix = "Bearer ";
+    private static readonly Regex ExtractTenantIdRegex = new(@"https:\/\/(?:sts\.windows\.net|login\.microsoftonline\.com)\/([a-z0-9-]{36}|[a-zA-Z0-9\.]+)/", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
+    private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler = new();
+    private readonly string _tenant;
     private readonly string _audience;
-    private readonly string _stsDiscoveryEndpoint;
+    private readonly ConfigurationManager<OpenIdConnectConfiguration> _configurationManager;
 
     public AzureADAuthenticationMatcher(string tenant, string audience)
     {
         _audience = Guard.NotNullOrEmpty(audience);
-        _stsDiscoveryEndpoint = string.Format(CultureInfo.InvariantCulture, "https://login.microsoftonline.com/{0}/.well-known/openid-configuration", Guard.NotNullOrEmpty(tenant));
+        _tenant = Guard.NotNullOrEmpty(tenant);
+
+        var stsDiscoveryEndpoint = string.Format(CultureInfo.InvariantCulture, "https://login.microsoftonline.com/{0}/.well-known/openid-configuration", _tenant);
+        _configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(stsDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever());
     }
 
     public string Name => nameof(AzureADAuthenticationMatcher);
@@ -55,19 +63,27 @@ internal class AzureADAuthenticationMatcher : IStringMatcher
 
         try
         {
-            var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(_stsDiscoveryEndpoint, new OpenIdConnectConfigurationRetriever());
-            var config = configManager.GetConfigurationAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            var config = _configurationManager.GetConfigurationAsync().ConfigureAwait(false).GetAwaiter().GetResult();
 
             var validationParameters = new TokenValidationParameters
             {
                 ValidAudience = _audience,
                 ValidIssuer = config.Issuer,
+                IssuerValidator =  (issuer, _, _) =>
+                {
+                    if (TryExtractTenantId(issuer, out var extractedTenant) && string.Equals(extractedTenant, _tenant, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return issuer;
+                    }
+
+                    throw new SecurityTokenInvalidIssuerException("tenant mismatch.");
+                },
                 IssuerSigningKeys = config.SigningKeys,
                 ValidateLifetime = true
             };
 
             // Throws an Exception as the token is invalid (expired, invalid-formatted, etc.)
-            new JwtSecurityTokenHandler().ValidateToken(token, validationParameters, out var _);
+            _jwtSecurityTokenHandler.ValidateToken(token, validationParameters, out _);
 
             return MatchScores.Perfect;
         }
@@ -81,6 +97,21 @@ internal class AzureADAuthenticationMatcher : IStringMatcher
     public virtual string GetCSharpCodeArguments()
     {
         throw new NotImplementedException();
+    }
+
+    // Handles: https://sts.windows.net/{tenant}/, https://login.microsoftonline.com/{tenant}/ or /v2.0
+    private static bool TryExtractTenantId(string issuer, [NotNullWhen(true)] out string? tenant)
+    {
+        var match = ExtractTenantIdRegex.Match(issuer);
+
+        if (match is { Success: true, Groups.Count: > 1 })
+        {
+            tenant = match.Groups[1].Value;
+            return string.IsNullOrEmpty(tenant);
+        }
+
+        tenant = null;
+        return false;
     }
 }
 #endif
