410a212a868b11ddd860a54209b5d27edeb51c55
Stef <stef.heyenrath@gmail.com>
2025-05-16 21:08:24 +0200

    Fix BodyParser to correctly check for json

--- src/WireMock.Net/Util/BodyParser.cs
+++ src/WireMock.Net/Util/BodyParser.cs
@@ -177,7 +177,7 @@ internal static class BodyParser
             }
 
             // If string is not null or empty, try to deserialize the string to a JObject
-            if (settings.DeserializeJson && !string.IsNullOrEmpty(data.BodyAsString))
+            if (settings.DeserializeJson && IsJson(data.BodyAsString))
             {
                 try
                 {
@@ -196,6 +196,18 @@ internal static class BodyParser
         }
 
         return data;
+    }
+
+    private static bool IsJson(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return false;
+        }
+
+        value = value.Trim();
+
+        return (value.StartsWith("{") && value.EndsWith("}")) || (value.StartsWith("[") && value.EndsWith("]"));
     }
 
     private static async Task<(string? ContentType, byte[] Bytes)> ReadBytesAsync(Stream stream, string? contentEncoding = null, bool decompressGZipAndDeflate = true)
